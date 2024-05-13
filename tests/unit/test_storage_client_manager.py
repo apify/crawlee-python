bfb7e3931738d99aca03eb5ab1aa725493d5003c
Vlada Dusek <v.dusek96@gmail.com>
2024-05-13 14:18:09 +0200

    update tests

--- tests/unit/test_storage_client_manager.py
+++ tests/unit/test_storage_client_manager.py
@@ -9,25 +9,30 @@ from crawlee.memory_storage_client import MemoryStorageClient
 from crawlee.storage_client_manager import StorageClientManager
 
 
-def test_returns_memory_storage_client_as_default() -> None:
-    storage_client = StorageClientManager.get_storage_client()
+@pytest.fixture()
+def scm_class() -> type[StorageClientManager]:
+    return StorageClientManager
+
+
+def test_returns_memory_storage_client_as_default(scm_class: type[StorageClientManager]) -> None:
+    storage_client = scm_class.get_storage_client()
     assert isinstance(storage_client, MemoryStorageClient), 'Should return the memory storage client by default'
 
 
-def test_returns_provided_local_client_for_non_cloud_environment() -> None:
+def test_returns_provided_local_client_for_non_cloud_environment(scm_class: type[StorageClientManager]) -> None:
     local_client = Mock(spec=BaseStorageClient)
-    StorageClientManager.set_local_client(local_client)
-    storage_client = StorageClientManager.get_storage_client()
+    scm_class.set_local_client(local_client)
+    storage_client = scm_class.get_storage_client()
     assert storage_client == local_client, 'Should return the local client when not in cloud'
 
 
-def test_returns_provided_cloud_client_for_cloud_environment() -> None:
+def test_returns_provided_cloud_client_for_cloud_environment(scm_class: type[StorageClientManager]) -> None:
     cloud_client = Mock(spec=BaseStorageClient)
-    StorageClientManager.set_cloud_client(cloud_client)
-    storage_client = StorageClientManager.get_storage_client(in_cloud=True)
+    scm_class.set_cloud_client(cloud_client)
+    storage_client = scm_class.get_storage_client(in_cloud=True)
     assert storage_client == cloud_client, 'Should return the cloud client when in cloud'
 
 
-def test_raises_error_when_no_cloud_client_provided() -> None:
+def test_raises_error_when_no_cloud_client_provided(scm_class: type[StorageClientManager]) -> None:
     with pytest.raises(RuntimeError, match='cloud client was not provided'):
-        StorageClientManager.get_storage_client(in_cloud=True)
+        scm_class.get_storage_client(in_cloud=True)
