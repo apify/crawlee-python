970b579315c3645f95034b6e5c43ec1c634cb475
Vlada Dusek <v.dusek96@gmail.com>
2024-05-29 11:26:34 +0200

    rm redundant check

--- tests/unit/playwright_crawler/test_playwright_crawler.py
+++ tests/unit/playwright_crawler/test_playwright_crawler.py
@@ -1,7 +1,6 @@
 from __future__ import annotations
 
 from typing import TYPE_CHECKING
-from unittest.mock import AsyncMock
 
 from crawlee.playwright_crawler import PlaywrightCrawler
 from crawlee.storages.request_list import RequestList
@@ -13,7 +12,6 @@ if TYPE_CHECKING:
 async def test_basic_request() -> None:
     request_provider = RequestList(['https://httpbin.org/'])
     crawler = PlaywrightCrawler(request_provider=request_provider)
-    handler = AsyncMock()
     result: dict = {}
 
     @crawler.router.default_handler
@@ -23,11 +21,9 @@ async def test_basic_request() -> None:
         result['page_url'] = context.page.url
         result['page_title'] = await context.page.title()
         result['page_content'] = await context.page.content()
-        await handler()
 
     await crawler.run()
 
-    assert handler.called
     assert result.get('request_url') == result.get('page_url') == 'https://httpbin.org/'
     assert 'httpbin' in result.get('page_title', '')
     assert '<html' in result.get('page_content', '')  # there is some HTML content
