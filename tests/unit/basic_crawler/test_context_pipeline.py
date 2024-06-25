dbce97e8ea0e4d885ab9949868eff3a37ff911f8
Jan Buchar <Teyras@gmail.com>
2024-06-25 13:28:39 +0200

    Lint

--- tests/unit/basic_crawler/test_context_pipeline.py
+++ tests/unit/basic_crawler/test_context_pipeline.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
-from dataclasses import dataclass
 import logging
+from dataclasses import dataclass
 from typing import AsyncGenerator
 from unittest.mock import AsyncMock
 
