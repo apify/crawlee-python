e927321ba2782012ab0de0d2d881de82c4fcd1e0
Vlada Dusek <v.dusek96@gmail.com>
2024-07-09 14:03:38 +0200

    doc: improve guide proxy management (#278)

---
+++ docs/guides/proxy_management_examples/session_playwright.py
@@ -0,0 +1,19 @@
+from crawlee.playwright_crawler import PlaywrightCrawler
+from crawlee.proxy_configuration import ProxyConfiguration
+
+
+async def main() -> None:
+    # Create a ProxyConfiguration object and pass it to the crawler.
+    proxy_configuration = ProxyConfiguration(
+        proxy_urls=[
+            'http://proxy-1.com/',
+            'http://proxy-2.com/',
+        ]
+    )
+    crawler = PlaywrightCrawler(
+        proxy_configuration=proxy_configuration,
+        use_session_pool=True,
+        persist_cookies_per_session=True,
+    )
+
+    # ...
