6235d5892826df9d266a88bdcad15ce51d3ba348
Vlada Dusek <v.dusek96@gmail.com>
2024-07-16 09:32:13 +0200

    fix: Pylance reportPrivateImportUsage errors

--- src/crawlee/autoscaling/__init__.py
+++ src/crawlee/autoscaling/__init__.py
@@ -1,3 +1,5 @@
 from .autoscaled_pool import AutoscaledPool, ConcurrencySettings
 from .snapshotter import Snapshotter
 from .system_status import SystemStatus
+
+__all__ = ['AutoscaledPool', 'ConcurrencySettings', 'Snapshotter', 'SystemStatus']
--- src/crawlee/events/__init__.py
+++ src/crawlee/events/__init__.py
@@ -1,2 +1,4 @@
 from .event_manager import EventManager
 from .local_event_manager import LocalEventManager
+
+__all__ = ['EventManager', 'LocalEventManager']
