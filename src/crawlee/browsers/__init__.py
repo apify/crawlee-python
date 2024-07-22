cd9afcaa492632dff1279f563a2891641ac3a669
Saurav Jain <souravjain540@gmail.com>
2024-07-22 17:10:58 +0530

    Apply suggestions from code review
    Co-authored-by: Jan Buchar <jan.buchar@apify.com>

--- src/crawlee/browsers/__init__.py
+++ src/crawlee/browsers/__init__.py
@@ -4,7 +4,7 @@ try:
     from .playwright_browser_plugin import PlaywrightBrowserPlugin
 except ImportError as exc:
     raise ImportError(
-        "To import anything from this subpacakge, you need to install the 'playwright' extra."
+        "To import anything from this subpackage, you need to install the 'playwright' extra."
         "For example, if you use pip, run `pip install 'crawlee[playwright]'`.",
     ) from exc
 
--- src/crawlee/playwright_crawler/__init__.py
+++ src/crawlee/playwright_crawler/__init__.py
@@ -3,7 +3,7 @@ try:
     from .types import PlaywrightCrawlingContext
 except ImportError as exc:
     raise ImportError(
-        "To import anything from this subpacakge, you need to install the 'playwright' extra."
+        "To import anything from this subpackage, you need to install the 'playwright' extra."
         "For example, if you use pip, run `pip install 'crawlee[playwright]'`.",
     ) from exc
 
