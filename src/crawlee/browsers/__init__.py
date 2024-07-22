0688bf1860534ab6b2a85dc850bf3d56507ab154
Saurav Jain <souravjain540@gmail.com>
2024-07-22 19:11:26 +0530

    bug: minor log fix (#341)
    ### Description
    
    <!-- The purpose of the PR, list of the changes, ... -->
    
    wrong command in the log: 'pip install crawlee[playwright]', it should
    be "pip install 'crawlee[playwright]'"
    
    ### Issues
    
    <!-- If applicable, reference any related GitHub issues -->
    
    - N/A
    
    ### Testing
    
    <!-- Describe the testing process for these changes -->
    
    - N/A
    
    ### Checklist
    
    - [ ] Changes are described in the `CHANGELOG.md`
    - [ ] CI passed
    
    ---------
    
    Co-authored-by: Jan Buchar <jan.buchar@apify.com>

--- src/crawlee/browsers/__init__.py
+++ src/crawlee/browsers/__init__.py
@@ -4,8 +4,8 @@ try:
     from .playwright_browser_plugin import PlaywrightBrowserPlugin
 except ImportError as exc:
     raise ImportError(
-        'To import anything from this subpacakge, you need to install the "playwright" extra. '
-        'For example, if you use pip, run "pip install crawlee[playwright]".',
+        "To import anything from this subpackage, you need to install the 'playwright' extra."
+        "For example, if you use pip, run `pip install 'crawlee[playwright]'`.",
     ) from exc
 
 __all__ = ['BrowserPool', 'PlaywrightBrowserController', 'PlaywrightBrowserPlugin']
