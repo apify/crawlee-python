16c67959af41b5043ccf91cf82167e8069f9d63e
Vlada Dusek <v.dusek96@gmail.com>
2024-08-02 14:12:16 +0200

    curl-cffi is optional dependency

--- src/crawlee/http_clients/__init__.py
+++ src/crawlee/http_clients/__init__.py
@@ -1,5 +1,12 @@
 from .base import BaseHttpClient, HttpCrawlingResult, HttpResponse
-from .curl_impersonate import CurlImpersonateHttpClient
 from .httpx import HttpxHttpClient
+
+try:
+    from .curl_impersonate import CurlImpersonateHttpClient
+except ImportError as exc:
+    raise ImportError(
+        "To import anything from this subpackage, you need to install the 'curl-impersonate' extra."
+        "For example, if you use pip, run `pip install 'crawlee[curl-impersonate]'`.",
+    ) from exc
 
 __all__ = ['BaseHttpClient', 'CurlImpersonateHttpClient', 'HttpCrawlingResult', 'HttpResponse', 'HttpxHttpClient']
