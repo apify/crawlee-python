ccd81454166ece68391cdffedb8efe9e663361d9
MS_Y <alstjr7375@daum.net>
2024-08-21 18:43:35 +0900

    fix: Relative URLS supports & If not a URL, pass #417 (#431)
    ### Description
    
    - If it starts with `//`, replace it with the same protocol.
    - To avoid unsupported protocols, such as `mailto` and `tel`, we only
    allow writing `http` and `https`.
    
    ### Issues
    
    - Closes: #417 
    
    ### Testing
    
    - Add a unit test for cases that start with `//`.
    
    ### Checklist
    
    - [x] CI passed

--- tests/unit/_utils/test_urls.py
+++ tests/unit/_utils/test_urls.py
@@ -10,6 +10,7 @@ def test_is_url_absolute() -> None:
     assert is_url_absolute('http://example.com/path') is True
     assert is_url_absolute('https://example.com/path') is True
     assert is_url_absolute('ftp://example.com/path') is True
+    assert is_url_absolute('//example.com/path') is False
     assert is_url_absolute('/path/to/resource') is False
     assert is_url_absolute('relative/path/to/resource') is False
     assert is_url_absolute('example.com/path') is False
@@ -18,6 +19,11 @@ def test_is_url_absolute() -> None:
 def test_convert_to_absolute_url() -> None:
     base_url = 'http://example.com'
     relative_url = '/path/to/resource'
+    absolute_url = convert_to_absolute_url(base_url, relative_url)
+    assert absolute_url == 'http://example.com/path/to/resource'
+
+    base_url = 'http://example.com'
+    relative_url = '//example.com/path/to/resource'
     absolute_url = convert_to_absolute_url(base_url, relative_url)
     assert absolute_url == 'http://example.com/path/to/resource'
 
