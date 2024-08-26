4b55a4bc6288092caeb0b5bdddcfe8a8c97bc68f
Jindřich Bär <jindrichbar@gmail.com>
2024-08-26 14:28:23 +0200

    docs: unpin Docusaurus version, fix build (#462)

--- website/src/theme/DocItem/Layout/index.js
+++ website/src/theme/DocItem/Layout/index.js
@@ -1,16 +1,17 @@
-import React from 'react';
-import clsx from 'clsx';
+import { useDoc } from '@docusaurus/plugin-content-docs/client';
 import { useWindowSize, useColorMode } from '@docusaurus/theme-common';
-import { useDoc } from '@docusaurus/theme-common/internal';
-import DocItemPaginator from '@theme/DocItem/Paginator';
-import DocVersionBanner from '@theme/DocVersionBanner';
-import DocVersionBadge from '@theme/DocVersionBadge';
-import DocItemFooter from '@theme/DocItem/Footer';
-import DocItemTOCMobile from '@theme/DocItem/TOC/Mobile';
-import DocItemTOCDesktop from '@theme/DocItem/TOC/Desktop';
-import DocItemContent from '@theme/DocItem/Content';
-import DocBreadcrumbs from '@theme/DocBreadcrumbs';
 import Giscus from '@giscus/react';
+import DocBreadcrumbs from '@theme/DocBreadcrumbs';
+import DocItemContent from '@theme/DocItem/Content';
+import DocItemFooter from '@theme/DocItem/Footer';
+import DocItemPaginator from '@theme/DocItem/Paginator';
+import DocItemTOCDesktop from '@theme/DocItem/TOC/Desktop';
+import DocItemTOCMobile from '@theme/DocItem/TOC/Mobile';
+import DocVersionBadge from '@theme/DocVersionBadge';
+import DocVersionBanner from '@theme/DocVersionBanner';
+import clsx from 'clsx';
+import React from 'react';
+
 import styles from './styles.module.css';
 
 /**
--- website/src/theme/DocSidebar/Desktop/Content/index.js
+++ website/src/theme/DocSidebar/Desktop/Content/index.js
@@ -1,44 +1,46 @@
-import React, {useState} from 'react';
-import clsx from 'clsx';
-import {ThemeClassNames} from '@docusaurus/theme-common';
+import { ThemeClassNames } from '@docusaurus/theme-common';
 import {
-  useAnnouncementBar,
-  useScrollPosition,
+    useAnnouncementBar,
+    useScrollPosition,
 } from '@docusaurus/theme-common/internal';
-import {translate} from '@docusaurus/Translate';
+import { translate } from '@docusaurus/Translate';
 import DocSidebarItems from '@theme/DocSidebarItems';
+import clsx from 'clsx';
+import React, { useState } from 'react';
+
 import styles from './styles.module.css';
+
 function useShowAnnouncementBar() {
-  const {isActive} = useAnnouncementBar();
-  const [showAnnouncementBar, setShowAnnouncementBar] = useState(isActive);
-  useScrollPosition(
-    ({scrollY}) => {
-      if (isActive) {
-        setShowAnnouncementBar(scrollY === 0);
-      }
-    },
-    [isActive],
-  );
-  return isActive && showAnnouncementBar;
+    const { isActive } = useAnnouncementBar();
+    const [showAnnouncementBar, setShowAnnouncementBar] = useState(isActive);
+    useScrollPosition(
+        ({ scrollY }) => {
+            if (isActive) {
+                setShowAnnouncementBar(scrollY === 0);
+            }
+        },
+        [isActive],
+    );
+    return isActive && showAnnouncementBar;
 }
-export default function DocSidebarDesktopContent({path, sidebar, className}) {
-  const showAnnouncementBar = useShowAnnouncementBar();
-  return (
-    <nav
-      aria-label={translate({
-        id: 'theme.docs.sidebar.navAriaLabel',
-        message: 'Docs sidebar',
-        description: 'The ARIA label for the sidebar navigation',
-      })}
-      className={clsx(
-        'menu thin-scrollbar',
-        styles.menu,
-        showAnnouncementBar && styles.menuWithAnnouncementBar,
-        className,
-      )}>
-      <ul className={clsx(ThemeClassNames.docs.docSidebarMenu, 'menu__list')}>
-        <DocSidebarItems items={sidebar} activePath={path} level={1} />
-      </ul>
-    </nav>
-  );
+export default function DocSidebarDesktopContent({ path, sidebar, className }) {
+    const showAnnouncementBar = useShowAnnouncementBar();
+    return (
+        <nav
+            aria-label={translate({
+                id: 'theme.docs.sidebar.navAriaLabel',
+                message: 'Docs sidebar',
+                description: 'The ARIA label for the sidebar navigation',
+            })}
+            className={clsx(
+                'menu thin-scrollbar',
+                styles.menu,
+                showAnnouncementBar && styles.menuWithAnnouncementBar,
+                className,
+            )}>
+            <ul className={clsx(ThemeClassNames.docs.docSidebarMenu, 'menu__list')}>
+                <DocSidebarItems items={sidebar} activePath={path} level={1} />
+            </ul>
+        </nav>
+    );
 }
