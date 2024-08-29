a1a792bc33154eb75cd6b0f8066e67a15cccb2c8
Jan Buchar <Teyras@gmail.com>
2024-08-29 15:17:23 +0200

    Add deployment guide

---
+++ docs/introduction/code/09_apify_sdk.py
@@ -0,0 +1,22 @@
+import asyncio
+
+from apify import Actor
+from crawlee.playwright_crawler import PlaywrightCrawler
+
+from .routes import router
+
+
+async def main() -> None:
+    async with Actor:
+        crawler = PlaywrightCrawler(
+            # Let's limit our crawls to make our tests shorter and safer.
+            max_requests_per_crawl=50,
+            # Provide our router instance to the crawler.
+            request_handler=router,
+        )
+
+        await crawler.run(['https://warehouse-theme-metal.myshopify.com/collections'])
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
