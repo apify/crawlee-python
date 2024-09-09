279e133904af493ddd3ad1d9d5a6cdb26c7c7967
Vlada Dusek <v.dusek96@gmail.com>
2024-09-09 15:57:59 +0200

    refactor: simplify type of request(s) args (#504)
    ### Description
    
    - Simplify type of request(s) args to only string or Request.
    - Should be only internal change, not breaking, as `BaseRequestData` was
    not exposed to the public in v0.3.
    
    ### Issues
    
    - Closes: N/A
    
    ### Testing
    
    - N/A
    
    ### Checklist
    
    - [x] CI passed

--- src/crawlee/storages/_request_provider.py
+++ src/crawlee/storages/_request_provider.py
@@ -4,7 +4,7 @@ from abc import ABC, abstractmethod
 from datetime import timedelta
 from typing import TYPE_CHECKING
 
-from crawlee._request import BaseRequestData, Request
+from crawlee._request import Request
 
 if TYPE_CHECKING:
     from collections.abc import Sequence
@@ -58,7 +58,7 @@ class RequestProvider(ABC):
     @abstractmethod
     async def add_requests_batched(
         self,
-        requests: Sequence[str | BaseRequestData | Request],
+        requests: Sequence[str | Request],
         *,
         batch_size: int = 1000,
         wait_time_between_batches: timedelta = timedelta(seconds=1),
@@ -75,7 +75,7 @@ class RequestProvider(ABC):
             wait_for_all_requests_to_be_added_timeout: Timeout for waiting for all requests to be added.
         """
 
-    def _transform_request(self, request: str | BaseRequestData | Request) -> Request:
+    def _transform_request(self, request: str | Request) -> Request:
         """Transforms a request-like object into a Request object."""
         if isinstance(request, Request):
             return request
@@ -83,12 +83,9 @@ class RequestProvider(ABC):
         if isinstance(request, str):
             return Request.from_url(request)
 
-        if isinstance(request, BaseRequestData):
-            return Request.from_base_request_data(request)
-
         raise ValueError(f'Invalid request type: {type(request)}')
 
-    def _transform_requests(self, requests: Sequence[str | BaseRequestData | Request]) -> list[Request]:
+    def _transform_requests(self, requests: Sequence[str | Request]) -> list[Request]:
         """Transforms a list of request-like objects into a list of Request objects."""
         processed_requests = dict[str, Request]()
 
