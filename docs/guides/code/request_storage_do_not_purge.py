aae11dd90206f40f887c2bbba325d89093aa96ef
Vlada Dusek <v.dusek96@gmail.com>
2024-09-13 18:59:40 +0200

    docs: add request storage guide
    Closes: #478

---
+++ docs/guides/code/request_storage_do_not_purge.py
@@ -0,0 +1,19 @@
+import asyncio
+
+from crawlee.configuration import Configuration
+from crawlee.http_crawler import HttpCrawler, HttpCrawlingContext
+
+
+async def main() -> None:
+    # highlight-next-line
+    config = Configuration(purge_on_start=False)
+    crawler = HttpCrawler(configuration=config)
+
+    @crawler.router.default_handler
+    async def request_handler(context: HttpCrawlingContext) -> None:
+        context.log.info(f'Processing {context.request.url} ...')
+
+    await crawler.run(['https://crawlee.dev/'])
+
+if __name__ == '__main__':
+    asyncio.run(main())
---
+++ docs/guides/code/request_storage_rl_with_crawler.py
@@ -0,0 +1,35 @@
+import asyncio
+
+from crawlee.http_crawler import HttpCrawler, HttpCrawlingContext
+from crawlee.storages import RequestList
+
+
+async def main() -> None:
+    # Open the request list, if it does not exist, it will be created.
+    # Leave name empty to use the default request list.
+    request_list = RequestList(
+        name='my-request-list',
+        requests=['https://apify.com/', 'https://crawlee.dev/'],
+    )
+
+    # Create a new crawler (it can be any subclass of BasicCrawler) and pass the request
+    # list as request provider to it. It will be managed by the crawler.
+    crawler = HttpCrawler(request_provider=request_list)
+
+    # Define the default request handler, which will be called for every request.
+    @crawler.router.default_handler
+    async def request_handler(context: HttpCrawlingContext) -> None:
+        context.log.info(f'Processing {context.request.url} ...')
+
+        # Use context's add_requests method helper to add new requests from the handler.
+        await context.add_requests(['https://crawlee.dev/python/docs/quick-start'])
+
+    # Use crawler's add_requests method helper to add new requests.
+    await crawler.add_requests(['https://crawlee.dev/python/api'])
+
+    # Run the crawler. You can optionally pass the list of initial requests.
+    await crawler.run(['https://crawlee.dev/python/'])
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
