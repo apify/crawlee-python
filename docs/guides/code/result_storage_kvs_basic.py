23f9be6971e53c7ce692cda94c017a0d3f26e50f
Manish Kumar <109733755+Manish-k723@users.noreply.github.com>
2024-11-05 20:15:50 +0530

    docs: Add a new guide for result storages (#587)
    ### Description
    
    This PR adds documentation on Crawlee's result storage types,
    specifically the Key-Value Store and Dataset, providing usage examples
    and file structures for efficient data management.<!-- The purpose of
    the PR, list of the changes, ... -->
    
    - Closes: #479  
    
    - [x] CI passed
    
    ---------
    
    Co-authored-by: Vlada Dusek <v.dusek96@gmail.com>

---
+++ docs/guides/code/result_storage_kvs_basic.py
@@ -0,0 +1,26 @@
+import asyncio
+
+from crawlee.storages import KeyValueStore
+
+
+async def main() -> None:
+    # Open the key-value store, if it does not exist, it will be created.
+    # Leave name empty to use the default KVS.
+    kvs = await KeyValueStore.open()
+
+    # Set a value associated with 'some-key'.
+    await kvs.set_value(key='some-key', value={'foo': 'bar'})
+
+    # Get the value associated with 'some-key'.
+    value = kvs.get_value('some-key')
+    # Do something with it...
+
+    # Delete the value associated with 'some-key' by setting it to None.
+    await kvs.set_value(key='some-key', value=None)
+
+    # Remove the key-value store.
+    await kvs.drop()
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
