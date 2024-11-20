50f5c7de33c51fe9d063d469abb3882646502607
Vlada Dusek <v.dusek96@gmail.com>
2024-11-20 11:25:55 +0100

    fix: Add init flag to the event manager

--- src/crawlee/events/_event_manager.py
+++ src/crawlee/events/_event_manager.py
@@ -80,9 +80,15 @@ class EventManager:
             delay=self._persist_state_interval,
         )
 
+        # Flag to indicate the context state.
+        self._initialized = False
+
     async def __aenter__(self) -> EventManager:
         """Initializes the event manager upon entering the async context."""
-        self._emit_persist_state_event_rec_task.start()
+        if not self._initialized:
+            self._emit_persist_state_event_rec_task.start()
+            self._initialized = True
+
         return self
 
     async def __aexit__(
@@ -95,11 +101,13 @@ class EventManager:
 
         This will stop listening for the events, and it will wait for all the event listeners to finish.
         """
-        await self.wait_for_all_listeners_to_complete(timeout=self._close_timeout)
-        self._event_emitter.remove_all_listeners()
-        self._listener_tasks.clear()
-        self._listeners_to_wrappers.clear()
-        await self._emit_persist_state_event_rec_task.stop()
+        if self._initialized:
+            await self.wait_for_all_listeners_to_complete(timeout=self._close_timeout)
+            self._event_emitter.remove_all_listeners()
+            self._listener_tasks.clear()
+            self._listeners_to_wrappers.clear()
+            await self._emit_persist_state_event_rec_task.stop()
+            self._initialized = False
 
     def on(self, *, event: Event, listener: Listener) -> None:
         """Add an event listener to the event manager.
@@ -164,6 +172,9 @@ class EventManager:
             event: The event which will be emitted.
             event_data: The data which will be passed to the event listeners.
         """
+        if not self._initialized:
+            raise RuntimeError('The event manager is not initialized. Use it within an async context.')
+
         self._event_emitter.emit(event.value, event_data)
 
     async def wait_for_all_listeners_to_complete(self, *, timeout: timedelta | None = None) -> None:
@@ -173,6 +184,8 @@ class EventManager:
             timeout: The maximum time to wait for the event listeners to finish. If they do not complete within
                 the specified timeout, they will be canceled.
         """
+        if not self._initialized:
+            raise RuntimeError('The event manager is not initialized. Use it within an async context.')
 
         async def wait_for_listeners() -> None:
             """Gathers all listener tasks and awaits their completion, logging any exceptions encountered."""
