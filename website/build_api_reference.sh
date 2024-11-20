018ca6668694e432b59b8a6d3eadfb9e503a199b
Jindřich Bär <jindrichbar@gmail.com>
2024-11-20 09:13:52 +0100

    docs: bump the docs plugin, unify the Python docs pipeline

--- website/build_api_reference.sh
+++ website/build_api_reference.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
 
-# Create docspec dump of this package's source code through pydoc-markdown
-python ./pydoc-markdown/generate_ast.py > docspec-dump.jsonl
-
-rm -rf "${apify_shared_tempdir}"
-
 # Generate import shortcuts from the modules
 python generate_module_shortcuts.py
-
-# Transform the docpec dumps into Typedoc-compatible docs tree
-node transformDocs.js
