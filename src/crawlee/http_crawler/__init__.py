0b0f4cefb35fdc0df02ed4ed937a1739b408f58f
Josef Prochazka <josef.prochazka@apify.com>
2024-11-27 14:54:44 +0100

    Review comments

--- src/crawlee/http_crawler/__init__.py
+++ src/crawlee/http_crawler/__init__.py
@@ -1,8 +1,10 @@
 from crawlee.http_clients import HttpCrawlingResult
+from crawlee.static_content_crawler._static_crawling_context import HttpCrawlingContext
 
 from ._http_crawler import HttpCrawler
 
 __all__ = [
     'HttpCrawler',
+    'HttpCrawlingContext',
     'HttpCrawlingResult',
 ]
--- src/crawlee/static_content_crawler/__init__.py
+++ src/crawlee/static_content_crawler/__init__.py
@@ -1,10 +1,9 @@
 from ._static_content_crawler import StaticContentCrawler
 from ._static_content_parser import BlockedInfo, StaticContentParser
-from ._static_crawling_context import HttpCrawlingContext, ParsedHttpCrawlingContext
+from ._static_crawling_context import ParsedHttpCrawlingContext
 
 __all__ = [
     'BlockedInfo',
-    'HttpCrawlingContext',
     'ParsedHttpCrawlingContext',
     'StaticContentCrawler',
     'StaticContentParser',
