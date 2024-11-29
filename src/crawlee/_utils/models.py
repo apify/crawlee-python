98d0eb5c3550251846be6b88c14d6ba5f6c0d7e4
Josef Prochazka <josef.prochazka@apify.com>
2024-11-29 13:38:05 +0100

    Allow serializer to output None

--- src/crawlee/_utils/models.py
+++ src/crawlee/_utils/models.py
@@ -1,25 +1,26 @@
-from __future__ import annotations
-
 from contextlib import suppress
 from datetime import timedelta
-from typing import Annotated, Any, Callable
+from typing import Annotated, Any, Callable, Union
 
 from pydantic import PlainSerializer, TypeAdapter, ValidationError, WrapValidator
 
 """Utility types for Pydantic models."""
 
 
-def _timedelta_to_ms(td: timedelta) -> float:
+def _timedelta_to_ms(td: Union[timedelta, None]) -> Union[float, None]:
     if td == timedelta.max:
         return float('inf')
-
+    if td is None:
+        return td
     return int(round(td.total_seconds() * 1000))
 
 
 _number_parser = TypeAdapter(float)
 
 
-def _timedelta_from_ms(value: float | timedelta | Any | None, handler: Callable[[Any], timedelta]) -> timedelta | None:
+def _timedelta_from_ms(
+    value: Union[float, timedelta, Any, None], handler: Callable[[Any], timedelta]
+) -> Union[timedelta, None]:
     if value == float('inf'):
         return timedelta.max
 
