6bba5527f1275d136a0d6a75d17736656570e9b3
Josef Prochazka <josef.prochazka@apify.com>
2024-12-04 16:55:26 +0100

    Move BlockedInfo to its own file.

--- src/crawlee/basic_crawler/__init__.py
+++ src/crawlee/basic_crawler/__init__.py
@@ -1,6 +1,7 @@
 from crawlee._types import BasicCrawlingContext
 
-from ._basic_crawler import BasicCrawler, BasicCrawlerOptions, BlockedInfo
+from ._basic_crawler import BasicCrawler, BasicCrawlerOptions
+from ._blocked_info import BlockedInfo
 from ._context_pipeline import ContextPipeline
 
 __all__ = ['BasicCrawler', 'BasicCrawlerOptions', 'BasicCrawlingContext', 'BlockedInfo', 'ContextPipeline']
