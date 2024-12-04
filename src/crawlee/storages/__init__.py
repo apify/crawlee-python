2f24705c30c915e77a853bc919efa0791ceae8fa
Jan Buchar <Teyras@gmail.com>
2024-12-04 13:45:28 +0100

    Fix import

--- src/crawlee/storages/__init__.py
+++ src/crawlee/storages/__init__.py
@@ -3,7 +3,7 @@ from ._key_value_store import KeyValueStore
 from ._request_list import RequestList
 from ._request_queue import RequestQueue
 from ._request_source import RequestSource
-from ._request_tandem import RequestSourceTandem
+from ._request_source_tandem import RequestSourceTandem
 
 __all__ = [
     'Dataset',
