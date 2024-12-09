c571caa4312c697252bef3b7f6a8c753d8677e82
Josef Prochazka <josef.prochazka@apify.com>
2024-12-09 17:09:23 +0100

    WIP

--- src/crawlee/_utils/html_to_text.py
+++ src/crawlee/_utils/html_to_text.py
@@ -1,7 +1,11 @@
 from __future__ import annotations
 
+import re
+
 from bs4 import BeautifulSoup
 
+SKIP_TAGS = {"script", "style" "canvas", "svg", "noscript"}
+BLOCK_TAGS = {"p" , "h1", "h2", "h3", "h4", "h5", "h6", "ol", "ul", "li", "pre", "address", "blockquote","dl", "div","fieldset", "form", "table" ,"tr","select","option"}
 
 def html_to_text(source: str | BeautifulSoup) -> str:
     """Converts markup string or BeautifulSoup object to newline separated plain text without tags."""
@@ -11,4 +15,8 @@ def html_to_text(source: str | BeautifulSoup) -> str:
         soup = source
     else:
         raise TypeError('Source must be either a string or a BeautifulSoup object.')
+    for tag in soup.findAll():
+        print(tag)
+        if tag.c
+
     return soup.get_text('\n', strip=True)
