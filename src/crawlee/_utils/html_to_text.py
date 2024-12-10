f26f8b6b7ef7ff0a1564e4f9f702c0a00c9c3197
Josef Prochazka <josef.prochazka@apify.com>
2024-12-10 17:38:41 +0100

    WIP follow JS implementation

--- src/crawlee/_utils/html_to_text.py
+++ src/crawlee/_utils/html_to_text.py
@@ -2,9 +2,10 @@ from __future__ import annotations
 
 import re
 
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, NavigableString, Tag, PageElement
 
-SKIP_TAGS = {"script", "style" "canvas", "svg", "noscript"}
+
+SKIP_TAGS = {"script", "style" "canvas", "svg", "noscript", "title"}
 BLOCK_TAGS = {"p" , "h1", "h2", "h3", "h4", "h5", "h6", "ol", "ul", "li", "pre", "address", "blockquote","dl", "div","fieldset", "form", "table" ,"tr","select","option"}
 
 def html_to_text(source: str | BeautifulSoup) -> str:
@@ -15,8 +16,40 @@ def html_to_text(source: str | BeautifulSoup) -> str:
         soup = source
     else:
         raise TypeError('Source must be either a string or a BeautifulSoup object.')
-    for tag in soup.findAll():
-        print(tag)
-        if tag.c
 
-    return soup.get_text('\n', strip=True)
+    text = ""
+
+    def _page_element_to_text(page_element: PageElement) -> str:
+        nonlocal text
+        if isinstance(page_element, NavigableString):
+            compr: str
+            if page_element.parent.name.lower() == 'pre':
+                compr = page_element.get_text()
+            else:
+                # Compares white spaces outside of pre block
+                compr = re.sub(r"\s+", " ", page_element.get_text())
+            if compr.startswith(" ") and re.match(r"^|\s", page_element.get_text()):
+                compr = compr[1:]
+            text += compr
+            if page_element.parent.name.lower() == 'br':
+                text += "\n"
+            if page_element.parent.name.lower() == 'td':
+                text += "\t"
+            if page_element.parent.name.lower() in BLOCK_TAGS:
+                text = f"\n{compr}"
+            return compr
+
+        if isinstance(page_element, Tag) and page_element.name.lower() in SKIP_TAGS:
+            return ""
+        x = list(page_element.stripped_strings)
+        text_parts = [_page_element_to_text(child) for child in page_element.children]
+
+        return "".join(text_parts)
+
+
+
+
+    return _page_element_to_text(soup)
+
+
+
