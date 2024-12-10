db0cc6509b016a257e22377a10ad682c0eb11fad
Josef Prochazka <josef.prochazka@apify.com>
2024-12-10 20:28:30 +0100

    Almost same as JS implementation. TODO: Fix last differences and add more tests according to JS implementation.

--- src/crawlee/_utils/html_to_text.py
+++ src/crawlee/_utils/html_to_text.py
@@ -5,7 +5,7 @@ import re
 from bs4 import BeautifulSoup, NavigableString, Tag, PageElement
 
 
-SKIP_TAGS = {"script", "style" "canvas", "svg", "noscript", "title"}
+SKIP_TAGS = {"script", "style", "canvas", "svg", "noscript", "title"}
 BLOCK_TAGS = {"p" , "h1", "h2", "h3", "h4", "h5", "h6", "ol", "ul", "li", "pre", "address", "blockquote","dl", "div","fieldset", "form", "table" ,"tr","select","option"}
 
 def html_to_text(source: str | BeautifulSoup) -> str:
@@ -19,37 +19,39 @@ def html_to_text(source: str | BeautifulSoup) -> str:
 
     text = ""
 
-    def _page_element_to_text(page_element: PageElement) -> str:
+    def _page_element_to_text(page_elements: PageElement) -> str:
         nonlocal text
-        if isinstance(page_element, NavigableString):
-            compr: str
-            if page_element.parent.name.lower() == 'pre':
-                compr = page_element.get_text()
-            else:
-                # Compares white spaces outside of pre block
-                compr = re.sub(r"\s+", " ", page_element.get_text())
-            if compr.startswith(" ") and re.match(r"^|\s", page_element.get_text()):
-                compr = compr[1:]
-            text += compr
-            if page_element.parent.name.lower() == 'br':
+        for page_element in page_elements:
+            if isinstance(page_element, NavigableString):
+                compr: str
+                if page_element.parent.name.lower() == 'pre':
+                    compr = page_element.get_text()
+                else:
+                    # Compares white spaces outside of pre block
+                    compr = re.sub(r"\s+", " ", page_element.get_text())
+                # If text is empty or ends with a whitespace, don't add the leading whitespace
+                if compr.startswith(" ") and re.search(r"(^|\s)$", text):
+                    compr = compr[1:]
+                text += compr
+            elif page_element.name.lower() in SKIP_TAGS or isinstance(page_element, int):
+                # Skip comments and special elements
+                pass
+            elif page_element.name.lower() == 'br':
                 text += "\n"
-            if page_element.parent.name.lower() == 'td':
+            elif page_element.name.lower() == 'td':
+                _page_element_to_text(page_element.children)
                 text += "\t"
-            if page_element.parent.name.lower() in BLOCK_TAGS:
-                text = f"\n{compr}"
-            return compr
+            else:
+                # Block elements must be surrounded by newlines(unless beginning of text)
+                is_block_tag = page_element.name.lower() in BLOCK_TAGS
+                if is_block_tag and not re.search(r"(^|\n)$", text):
+                    text += '\n'
+                _page_element_to_text(page_element.children)
+                if (is_block_tag and not text.endswith('\n')):
+                    text += '\n'
 
-        if isinstance(page_element, Tag) and page_element.name.lower() in SKIP_TAGS:
-            return ""
-        x = list(page_element.stripped_strings)
-        text_parts = [_page_element_to_text(child) for child in page_element.children]
-
-        return "".join(text_parts)
-
-
-
-
-    return _page_element_to_text(soup)
+    _page_element_to_text(soup)
+    return text.strip()
 
 
 
