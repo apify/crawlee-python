210749e6d6ef495cf1a9d08e536677f4e3130efc
Josef Prochazka <josef.prochazka@apify.com>
2024-12-17 10:56:44 +0100

    Add docs decorator

--- src/crawlee/utils.py
+++ src/crawlee/utils.py
@@ -1,8 +1,17 @@
 from typing import Callable
 
+from crawlee._utils.docs import docs_group
 
+
+@docs_group('Functions')
 def html_to_text(source: str) -> str:
-    """Converts markup string to newline separated plain text without tags."""
+    """Converts markup string to newline separated plain text without tags.
+
+    Args:
+        source: Input markup string
+    Returns:
+        Newline separated plain text without tags.
+    """
     _html_to_text: Callable[[str], str]
     try:
         from crawlee.beautifulsoup_crawler._utils import html_to_text as _html_to_text
