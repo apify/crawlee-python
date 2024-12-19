3a907ec07b537b53a9f1611907961cd9a2a8598f
Jan Buchar <Teyras@gmail.com>
2024-12-19 17:01:09 +0100

    Shorten imports

--- src/crawlee/basic_crawler/_basic_crawler.py
+++ src/crawlee/basic_crawler/_basic_crawler.py
@@ -49,13 +49,12 @@ if TYPE_CHECKING:
     from contextlib import AbstractAsyncContextManager
 
     from crawlee._types import ConcurrencySettings, HttpMethod, JsonSerializable
-    from crawlee.base_storage_client import BaseStorageClient
-    from crawlee.base_storage_client._models import DatasetItemsListPage
+    from crawlee.base_storage_client import BaseStorageClient, DatasetItemsListPage
     from crawlee.configuration import Configuration
-    from crawlee.events._event_manager import EventManager
+    from crawlee.events import EventManager
     from crawlee.http_clients import BaseHttpClient, HttpResponse
     from crawlee.proxy_configuration import ProxyConfiguration, ProxyInfo
-    from crawlee.request_loaders._request_manager import RequestManager
+    from crawlee.request_loaders import RequestManager
     from crawlee.sessions import Session
     from crawlee.statistics import FinalStatistics, StatisticsState
     from crawlee.storages._dataset import ExportDataCsvKwargs, ExportDataJsonKwargs, GetDataKwargs, PushDataKwargs
