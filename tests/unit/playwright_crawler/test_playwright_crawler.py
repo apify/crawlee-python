6e35c1df76c460de2e28b67ce93db56b0f2b3d20
Josef Prochazka <josef.prochazka@apify.com>
2024-12-19 11:11:13 +0100

    Remove unintentional change to headless test

--- tests/unit/playwright_crawler/test_playwright_crawler.py
+++ tests/unit/playwright_crawler/test_playwright_crawler.py
@@ -114,7 +114,7 @@ async def test_chromium_headless_headers(httpbin: URL) -> None:
 
 
 async def test_firefox_headless_headers(httpbin: URL) -> None:
-    crawler = PlaywrightCrawler(headless=False, browser_type='firefox')
+    crawler = PlaywrightCrawler(headless=True, browser_type='firefox')
     headers = dict[str, str]()
 
     @crawler.router.default_handler
