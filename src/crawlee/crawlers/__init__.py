e05a3f0ba662432f0e3371b8486918a5cef1e914
Vlada Dusek <v.dusek96@gmail.com>
2024-12-20 16:26:38 +0100

    crawlers imports

--- src/crawlee/crawlers/__init__.py
+++ src/crawlee/crawlers/__init__.py
@@ -1,7 +1,4 @@
 import contextlib as _contextlib
-import sys as _sys
-from types import ModuleType as _ModuleType
-from typing import Any as _Any
 
 from ._abstract_http import AbstractHttpCrawler, AbstractHttpParser, HttpCrawlerOptions, ParsedHttpCrawlingContext
 from ._basic import BasicCrawler, BasicCrawlerOptions, BasicCrawlingContext, BlockedInfo, ContextPipeline
@@ -15,16 +12,3 @@ with _contextlib.suppress(ImportError):
 
 with _contextlib.suppress(ImportError):
     from ._playwright import PlaywrightCrawler, PlaywrightCrawlingContext, PlaywrightPreNavCrawlingContext
-
-
-class _ModuleWrapper(_ModuleType):
-    def __getattribute__(self, attr: str) -> _Any:
-        result = super().__getattribute__(attr)
-
-        if isinstance(result, Exception):
-            raise result
-
-        return result
-
-
-_sys.modules[__name__].__class__ = _ModuleWrapper
