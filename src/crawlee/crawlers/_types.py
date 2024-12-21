100de63c38ca42a905a8f73d2a3ecf843a2e9162
Vlada Dusek <v.dusek96@gmail.com>
2024-12-21 11:11:31 +0100

    chore: Fix new dir structure-related issues

--- src/crawlee/crawlers/_types.py
+++ src/crawlee/crawlers/_types.py
@@ -2,10 +2,7 @@ from __future__ import annotations
 
 from dataclasses import dataclass
 
-from crawlee._utils.docs import docs_group
 
-
-@docs_group('Data structures')
 @dataclass(frozen=True)
 class BlockedInfo:
     """Information about whether the crawling is blocked. If reason is empty, then it means it is not blocked."""
