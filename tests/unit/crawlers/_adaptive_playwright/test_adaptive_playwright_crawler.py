0ecb137dccddf1cfb43c6204fb0a26923c1fc32b
Josef Prochazka <josef.prochazka@apify.com>
2025-01-06 14:06:15 +0100

    Add tests for use_state and predictor.

--- tests/unit/crawlers/_adaptive_playwright/test_adaptive_playwright_crawler.py
+++ tests/unit/crawlers/_adaptive_playwright/test_adaptive_playwright_crawler.py
@@ -1,13 +1,15 @@
 from __future__ import annotations
 
 from itertools import cycle
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, cast
+from unittest.mock import Mock, patch
 
 import pytest
 from typing_extensions import override
 
+from crawlee import Request
 from crawlee._types import BasicCrawlingContext
-from crawlee.crawlers import PlaywrightPreNavCrawlingContext
+from crawlee.crawlers import BasicCrawler, PlaywrightPreNavCrawlingContext
 from crawlee.crawlers._adaptive_playwright import AdaptivePlaywrightCrawler, AdaptivePlaywrightCrawlingContext
 from crawlee.crawlers._adaptive_playwright._adaptive_playwright_crawling_context import AdaptiveContextError
 from crawlee.crawlers._adaptive_playwright._rendering_type_predictor import (
@@ -24,10 +26,10 @@ if TYPE_CHECKING:
 class _SimpleRenderingTypePredictor(RenderingTypePredictor):
     """Simplified predictor for tests."""
 
-    def __init__(self, rendering_types: Iterator[RenderingType],
-                 detection_probability_recommendation: Iterator[int]) -> None:
-        self._rendering_types = rendering_types
-        self._detection_probability_recommendation = detection_probability_recommendation
+    def __init__(self, rendering_types: Iterator[RenderingType] | None = None,
+                 detection_probability_recommendation: None |  Iterator[int] = None) -> None:
+        self._rendering_types = rendering_types or  cycle(['static'])
+        self._detection_probability_recommendation = detection_probability_recommendation or cycle([1])
 
     @override
     def predict(self, url: str, label: str | None) -> RenderingTypePrediction:
@@ -41,10 +43,10 @@ class _SimpleRenderingTypePredictor(RenderingTypePredictor):
 
 @pytest.mark.parametrize(('expected_pw_count', 'expected_bs_count', 'rendering_types',
                           'detection_probability_recommendation'), [
-    pytest.param(0,2, cycle(['static']), cycle([0]), id='Static only.'),
-    pytest.param(2,0, cycle(['client only']), cycle([0]), id='Client only.'),
-    pytest.param(1,1, cycle(['static','client only']), cycle([0]),id='Mixed.'),
-    pytest.param(2,2, cycle(['static','client only']), cycle([1]),id='Enforced rendering type detection.'),
+    pytest.param(0,2, cycle(['static']), cycle([0]), id='Static only'),
+    pytest.param(2,0, cycle(['client only']), cycle([0]), id='Client only'),
+    pytest.param(1,1, cycle(['static','client only']), cycle([0]),id='Mixed'),
+    pytest.param(2,2, cycle(['static','client only']), cycle([1]),id='Enforced rendering type detection'),
 ])
 async def test_adaptive_crawling(expected_pw_count: int, expected_bs_count: int,
                                  rendering_types: Iterator[RenderingType],
@@ -52,13 +54,13 @@ async def test_adaptive_crawling(expected_pw_count: int, expected_bs_count: int,
     """Tests correct routing to pre-nav hooks and correct handling through proper handler."""
     requests = ['https://crawlee.dev/', 'https://crawlee.dev/docs/quick-start']
 
-    static_only_predictor = _SimpleRenderingTypePredictor(
+    predictor = _SimpleRenderingTypePredictor(
         rendering_types = rendering_types,
         detection_probability_recommendation=detection_probability_recommendation
     )
 
 
-    crawler = AdaptivePlaywrightCrawler(rendering_type_predictor=static_only_predictor)
+    crawler = AdaptivePlaywrightCrawler(rendering_type_predictor=predictor)
 
     pw_handler_count = 0
     bs_handler_count = 0
@@ -100,16 +102,10 @@ async def test_adaptive_crawling(expected_pw_count: int, expected_bs_count: int,
 
 
 async def test_adaptive_crawling_context() -> None:
-    """Tests that correct context is used."""
+    """Tests that correct context is used. Enforced rendering type detection to run both sub crawlers."""
+    static_only_predictor_enforce_detection = _SimpleRenderingTypePredictor()
     requests = ['https://crawlee.dev/']
-
-    static_only_predictor = _SimpleRenderingTypePredictor(
-        rendering_types = cycle(['static']),
-        detection_probability_recommendation=cycle([1])
-    )
-
-    crawler = AdaptivePlaywrightCrawler(rendering_type_predictor=static_only_predictor)
-
+    crawler = AdaptivePlaywrightCrawler(rendering_type_predictor=static_only_predictor_enforce_detection)
 
     @crawler.router.default_handler
     async def request_handler(context: AdaptivePlaywrightCrawlingContext) -> None:
@@ -126,3 +122,92 @@ async def test_adaptive_crawling_context() -> None:
         assert context.request.url == requests[0]
 
     await crawler.run(requests)
+
+
+async def test_adaptive_crawling_result() -> None:
+    """Tests that result only from one sub crawler is saved.
+
+    Enforced rendering type detection to run both sub crawlers."""
+    static_only_predictor_enforce_detection = _SimpleRenderingTypePredictor()
+    requests = ['https://crawlee.dev/']
+    crawler = AdaptivePlaywrightCrawler(rendering_type_predictor=static_only_predictor_enforce_detection)
+
+
+    @crawler.router.default_handler
+    async def request_handler(context: AdaptivePlaywrightCrawlingContext) -> None:
+        try:
+            # page is available only if it was crawled by PlaywrightCrawler.
+            context.page  # noqa:B018 Intentionally "useless expression". Can trigger exception.
+            await context.push_data({'handler': 'pw'})
+        except AdaptiveContextError:
+            await context.push_data({'handler': 'bs'})
+
+    await crawler.run(requests)
+
+    dataset = await crawler.get_dataset()
+    items = [item async for item in dataset.iterate_items()]
+
+    # Enforced rendering type detection will trigger both sub crawlers, but only pw crawler result is saved.
+    assert items == [{'handler': 'pw'}]
+
+
+
+@pytest.mark.parametrize(('pw_saved_data', 'bs_saved_data', 'expected_result_renderingl_type'), [
+    pytest.param({'some': 'data'}, {'some': 'data'}, 'static', id='Same results from sub crawlers'),
+    pytest.param({'some': 'data'}, {'different': 'data'}, 'client only', id='Different results from sub crawlers'),
+])
+async def test_adaptive_crawling_predictor_calls(pw_saved_data: dict[str, str], bs_saved_data: dict[str, str],
+                                                 expected_result_renderingl_type: RenderingType) -> None:
+    """Tests expected predictor calls. Same results."""
+    some_label = 'bla'
+    some_url = 'https://crawlee.dev/'
+    static_only_predictor_enforce_detection = _SimpleRenderingTypePredictor()
+    requests = [Request.from_url(url=some_url, label=some_label)]
+    crawler = AdaptivePlaywrightCrawler(rendering_type_predictor=static_only_predictor_enforce_detection)
+
+
+    @crawler.router.default_handler
+    async def request_handler(context: AdaptivePlaywrightCrawlingContext) -> None:
+        try:
+            # page is available only if it was crawled by PlaywrightCrawler.
+            context.page  # noqa:B018 Intentionally "useless expression". Can trigger exception.
+            await context.push_data(pw_saved_data)
+        except AdaptiveContextError:
+            await context.push_data(bs_saved_data)
+
+    with (patch.object(static_only_predictor_enforce_detection, 'store_result', Mock()) as mocked_store_result,
+        patch.object(static_only_predictor_enforce_detection, 'predict', Mock(
+            return_value=RenderingTypePrediction('static', 1))) as mocked_predict):
+        await crawler.run(requests)
+
+    mocked_predict.assert_called_once_with(some_url, some_label)
+    # If `static` and `client only` results are same, `store_result` should be called with `static`.
+    mocked_store_result.assert_called_once_with(some_url, some_label, expected_result_renderingl_type)
+
+
+async def test_adaptive_crawling_result_use_state_isolation() -> None:
+    """Tests that global state accessed through `use_state` is changed only by one sub crawler.
+
+    Enforced rendering type detection to run both sub crawlers."""
+    static_only_predictor_enforce_detection = _SimpleRenderingTypePredictor()
+    requests = ['https://crawlee.dev/']
+    crawler = AdaptivePlaywrightCrawler(rendering_type_predictor=static_only_predictor_enforce_detection)
+    store = await crawler.get_key_value_store()
+    await store.set_value(BasicCrawler.CRAWLEE_STATE_KEY, {'counter': 0})
+    request_handler_calls = 0
+
+    @crawler.router.default_handler
+    async def request_handler(context: AdaptivePlaywrightCrawlingContext) -> None:
+        nonlocal request_handler_calls
+        state = cast(dict[str, int],await context.use_state())
+        request_handler_calls += 1
+        state['counter'] += 1
+
+    await crawler.run(requests)
+
+    await store.persist_autosaved_values()
+
+    # Request handler was called twice
+    assert request_handler_calls == 2
+    # Increment of global state happened only once
+    assert (await store.get_value(BasicCrawler.CRAWLEE_STATE_KEY))['counter'] == 1
