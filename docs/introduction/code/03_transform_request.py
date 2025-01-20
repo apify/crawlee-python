063f7db0e2e9eff2d4ce403f50e60026e368b2c3
Max Bohomolov <moriturus7@gmail.com>
2025-01-20 17:00:35 +0000

    update doc

--- docs/introduction/code/03_transform_request.py
+++ docs/introduction/code/03_transform_request.py
@@ -4,23 +4,24 @@ from crawlee import HttpHeaders, Request
 from crawlee.crawlers import BeautifulSoupCrawler, BeautifulSoupCrawlingContext
 
 
+def transform_request(request: Request) -> Request | None:
+    # Skip requests to PDF files
+    if request.url.endswith('.pdf'):
+        return None
+
+    # Add custom headers to requests to specific sections
+    if '/docs' in request.url:
+        request.headers = HttpHeaders({'Custom-Header': 'value'})
+
+    # Add label for certain URLs
+    if '/blog' in request.url:
+        request.user_data['label'] = 'BLOG'
+
+    return request
+
+
 async def main() -> None:
     crawler = BeautifulSoupCrawler(max_requests_per_crawl=50)
-
-    def transform_request(request: Request) -> Request | None:
-        # Skip requests to PDF files
-        if request.url.endswith('.pdf'):
-            return None
-
-        # Add custom headers to requests to specific sections
-        if '/docs' in request.url:
-            request.headers = HttpHeaders({'Custom-Header': 'value'})
-
-        # Add label for certain URLs
-        if '/blog' in request.url:
-            request.user_data['label'] = 'BLOG'
-
-        return request
 
     @crawler.router.default_handler
     async def request_handler(context: BeautifulSoupCrawlingContext) -> None:
