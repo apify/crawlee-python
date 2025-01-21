1ce4d5de22751507570f8f6b0613935ecc507b6f
Josef Prochazka <josef.prochazka@apify.com>
2025-01-21 16:23:24 +0100

    Add predictor similarity comparison internals

--- src/crawlee/crawlers/_adaptive_playwright/_rendering_type_predictor.py
+++ src/crawlee/crawlers/_adaptive_playwright/_rendering_type_predictor.py
@@ -1,17 +1,18 @@
 from abc import ABC, abstractmethod
+from collections import defaultdict
 from dataclasses import dataclass
 from itertools import zip_longest
+from statistics import mean
 from typing import Literal, override
-
-from jaro import jaro_winkler_metric
-from sklearn.linear_model import LogisticRegression
 from urllib.parse import urlparse
 
 import jaro
-jaro.jaro_winkler_metric(u'SHACKLEFORD', u'SHACKELFORD')
+from jaro import jaro_winkler_metric
+from sklearn.linear_model import LogisticRegression
+
+jaro.jaro_winkler_metric('SHACKLEFORD', 'SHACKELFORD')
 
 from crawlee import Request
-
 
 """
 X = [ [f"{i}"] for i in range(0,100)]
@@ -21,7 +22,7 @@ point = [[49],]
 print(clf.predict(point))
 print(clf.predict_proba(point))
 """
-
+UrlComponents = list[str]
 RenderingType = Literal['static', 'client only']
 
 
@@ -44,46 +45,59 @@ class RenderingTypePredictor(ABC):
 class LogisticalRegressionPredictor(RenderingTypePredictor):
 
     def __init__(self):
+        self._rendering_type_detection_results: dict[RenderingType, dict[str, UrlComponents]] = {
+            'static': defaultdict(list[str]),
+            'client only': defaultdict(list[str]),
+        }
         self._inputs = []
         self._outputs = []
         self._model = LogisticRegression(random_state=0)
 
     @override
     def predict(self, request: Request) -> RenderingTypePrediction:
-        self._model.predict([request.url, request.label])
+        url_feature =''
+        #self._model.predict([request.url, request.label])
 
     @override
     def store_result(self, request: Request, rendering_type: RenderingType) -> None:
-        self._inputs.append([request.url, request.label])
-        self._outputs.append(rendering_type)
-        self._model.fit(self._inputs, self._outputs)
+        self._rendering_type_detection_results[rendering_type][request.label].append(get_url_components(request.url))
+        #self._model.fit(self._inputs, self._outputs)
+
+    def _get_mean_similarity(self, url: str, label: str, rendering_type: RenderingType) -> float:
+        return mean(
+            calculate_url_similarity(get_url_components(url), known_url_components)
+            for known_url_components in self._rendering_type_detection_results[rendering_type][label]
+        )
+
+    def _calculate_feature_vector(self, request: Request) -> (float, float):
+        return (
+            self._get_mean_similarity(request.url, request.label, 'static'),
+            self._get_mean_similarity(request.url, request.label, 'client only')
+        )
 
 
-def calculate_similarity(url_1: str, url_2: str) -> float:
-    """Calculate url similarity.
 
-    Return 0 if different host names
+
+
+def get_url_components(url: str) -> UrlComponents:
+    """Get list of url components where first component is host name."""
+    parsed_url = urlparse(url)
+    return [parsed_url.netloc, *parsed_url.path.strip('/').split('/')]
+
+def calculate_url_similarity(url_1: UrlComponents, url_2: UrlComponents) -> float:
+    """Calculate url similarity based on host name and path components similarity.
+
+    Return 0 if different host names.
     Compare path components using jaro-wrinkler method and assign 1 or 0 value based on similarity_cutoff for each
-    path component. Return their weighted average."""
+    path component. Return their weighted average.
+    """
+    # Anything with jaro_winkler_metric less than this value is considered completely different,
+    # otherwise considered the same.
+    similarity_cutoff = 0.8
 
-    similarity_cutoff = 0.8 # Anything not at least similar with this value is considered completely different.
-    parsed_url_1 = urlparse(url_1)
-    parsed_url_2 = urlparse(url_2)
-
-
-    if parsed_url_1.netloc != parsed_url_2.netloc:
+    if (url_1[0] != url_2[0]) or not url_1 or not url_2:
         return 0
 
-    url_1_paths = parsed_url_1.path.strip("/").split("/")
-    url_2_paths = parsed_url_2.path.strip("/").split("/")
-
-    similarity_sum = 0
-
-    for path_1, path_2 in zip_longest(url_1_paths, url_2_paths, fillvalue=""):
-        similarity_sum += 1 if jaro_winkler_metric(path_1, path_2) > similarity_cutoff else 0
-
-    return similarity_sum / max(len(url_1_paths), len(url_2_paths))
-
-
-print(calculate_similarity("https://docs.python.org/3/library/itertools.html#itertools.zip_longest",
-                           "https://docs.python.org/3.7/library/itertools.html#itertools.zip_longest"))
+    # Each additional path component from longer path is replaced by empty string in the shorter path.
+    return mean(1 if jaro_winkler_metric(path_1, path_2) > similarity_cutoff else 0 for
+         path_1, path_2 in zip_longest(url_1[1:], url_2[1:], fillvalue=''))
--- tests/unit/crawlers/_adaptive_playwright/test_predictor.py
+++ tests/unit/crawlers/_adaptive_playwright/test_predictor.py
@@ -1,10 +1,12 @@
-from dataclasses import dataclass
 
 import pytest
 
 from crawlee import Request
-from crawlee.crawlers._adaptive_playwright._rendering_type_predictor import LogisticalRegressionPredictor, \
-    calculate_similarity
+from crawlee.crawlers._adaptive_playwright._rendering_type_predictor import (
+    LogisticalRegressionPredictor,
+    calculate_url_similarity,
+    get_url_components,
+)
 
 
 def test_predictor():
@@ -13,29 +15,32 @@ def test_predictor():
 
     label_relevant_learning_data = (
         # Label "static for sure" should be enough for the predictor to predict "static"
-        ("http://www.aaa.com", "static for sure", "static"),
-        ("http://www.aab.com", "static for sure", "static"),
-        ("http://www.aac.com", "static for sure", "static"),
+        ('http://www.aaa.com', 'static for sure', 'static'),
+        ('http://www.aab.com', 'static for sure', 'static'),
+        ('http://www.aac.com', 'static for sure', 'static'),
         # Label "browser for sure" should be enough for the predictor to predict "static"
-        ("http://www.ddd.com", "browser for sure", "browser"),
-        ("http://www.dde.com", "browser for sure", "browser"),
-        ("http://www.ddf.com", "browser for sure", "browser"),
+        ('http://www.ddd.com', 'browser for sure', 'browser'),
+        ('http://www.dde.com', 'browser for sure', 'browser'),
+        ('http://www.ddf.com', 'browser for sure', 'browser'),
     )
 
     for learning_data in label_relevant_learning_data:
-        predictor.store_result(Request.from_url(url="http://www.aaa.com", label="static for sure"), rendering_type=learning_data[2])
+        predictor.store_result(Request.from_url(url='http://www.aaa.com', label='static for sure'), rendering_type=learning_data[2])
 
-    assert predictor.predict("http://www.aaa.com", "static for sure") == "static"
-    assert predictor.predict("http://www.ijk.com", "static for sure") == "static"
-    assert predictor.predict("http://www.ddd.com", "browser for sure") == "browser"
-    assert predictor.predict("http://www.ijk.com", "browser for sure") == "browser"
+    assert predictor.predict('http://www.aaa.com', 'static for sure') == 'static'
+    assert predictor.predict('http://www.ijk.com', 'static for sure') == 'static'
+    assert predictor.predict('http://www.ddd.com', 'browser for sure') == 'browser'
+    assert predictor.predict('http://www.ijk.com', 'browser for sure') == 'browser'
 
 
-@pytest.mark.parametrize(("url_1", "url_2", "expected_rounded_metric"), [
-    ("https://docs.python.org/3/library/itertools.html#itertools.zip_longest",
-     "https://docs.python.org/3.7/library/itertools.html#itertools.zip_longest", 0.67),
-    ("https://differente.com/same", "https://differenta.com/same", 0),
-    ("https://different.com/almost_the_same", "https://different.com/almost_the_sama", 1)
+@pytest.mark.parametrize(('url_1', 'url_2', 'expected_rounded_similarity'), [
+    ('https://docs.python.org/3/library/itertools.html#itertools.zip_longest',
+     'https://docs.python.org/3.7/library/itertools.html#itertools.zip_longest', 0.67),
+    ('https://differente.com/same', 'https://differenta.com/same', 0),
+    ('https://same.com/almost_the_same', 'https://same.com/almost_the_sama', 1),
+    ('https://same.com/same/extra', 'https://same.com/same', 0.5)
 ])
-def test_url_similarity(url_1: str, url_2: str, expected_rounded_metric: float):
-    assert round(calculate_similarity(url_1=url_1, url_2=url_2),2) == expected_rounded_metric
+def test_url_similarity(url_1: str, url_2: str, expected_rounded_similarity: float):
+    assert round(calculate_url_similarity(
+        url_1=get_url_components(url_1),
+        url_2=get_url_components(url_2)), 2) == expected_rounded_similarity
