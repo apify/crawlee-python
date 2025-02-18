682eacc5cd543223a7851acc625780d733be5316
Josef Prochazka <josef.prochazka@apify.com>
2025-02-18 17:37:21 +0100

    Basic structure
    TODO:
     Message matching and replacing with wildcards
     Extra args - full stack and full message
     Tests

--- src/crawlee/statistics/_error_tracker.py
+++ src/crawlee/statistics/_error_tracker.py
@@ -2,35 +2,59 @@
 
 from __future__ import annotations
 
+import traceback
 from collections import Counter, defaultdict
 from dataclasses import dataclass
 
 
-@dataclass(frozen=True, unsafe_hash=True)
-class ErrorGroup:
-    """Identifies a group of similar errors."""
 
-    class_name: str | None
+# Errors are internally tracked in deeply nested dict where individual error groups count can be accessed like this:
+# count = ErrorStackTraceGroups["some_stack_trace"]["some_error_code"]["some_error_name"]["some_error_message"]
+# if any of the filtering keys was not used it will be `None` instead.
+# count = ErrorStackTraceGroups["some_stack_trace"][None]["some_error_name"][None]
+
+ErrorMessageGroups = Counter[str|None]
+ErrorTypeGroups = dict[str|None, ErrorMessageGroups]
+#ErrorCodeGroups = dict[str|None, ErrorNameGroups] # No error codes in Python
+ErrorStackTraceGroups = dict[str|None, ErrorTypeGroups]
+
 
 
 class ErrorTracker:
     """Track errors and aggregates their counts by similarity."""
 
     def __init__(self,
-                 show_error_code: bool = True,
                  show_error_name: bool = True,
                  show_stack_trace: bool = True,
-                 show_full_stack: bool = False,
+                 show_full_stack: bool = False, # Todo, what does it do in JS?
                  show_error_message: bool = True,
-                 show_full_message: bool = False,
-                 save_error_snapshots: bool = False,
+                 show_full_message: bool = False, # Todo, seems like only first line otherwise
+                 save_error_snapshots: bool = False, # Todo, what does it do in JS?
     ) -> None:
-        self._errors = Counter[ErrorGroup]()
+        self.show_error_name = show_error_name
+        self.show_stack_trace = show_stack_trace
+        self.show_full_stack = show_full_stack
+        self.show_error_message = show_error_message
+        self.show_full_message = show_full_message
+
+        self._errors: ErrorStackTraceGroups = defaultdict(lambda: defaultdict(Counter))
 
     def add(self, error: Exception) -> None:
         """Include an error in the statistics."""
-        error_group = ErrorGroup(class_name=error.__class__.__name__)
-        self._errors[error_group] += 1
+        # Check for similar
+        error_group_name = error.__class__.__name__ if self.show_error_name else None
+        error_group_message = str(error) if self.show_error_message else None
+        if self.show_stack_trace:
+            error_traceback = traceback.extract_tb(error.__traceback__)
+            error_group_stack_trace = f"{error_traceback[0].filename}:{error_traceback[0].lineno}"
+            # More extraction if full stack...
+        else:
+            error_group_stack_trace = None
+
+        # TODO perform special message grouping
+        # First just exact match
+
+        self._errors[error_group_stack_trace][error_group_name].update([error_group_message])
 
     @property
     def unique_error_count(self) -> int:
@@ -41,3 +65,9 @@ class ErrorTracker:
     def total(self) -> int:
         """Total number of errors."""
         return sum(self._errors.values())
+
+    def _add_to_existing_similar_error_group(self):
+        pass
+
+    def _are_errors_similar(self) -> bool:
+        pass
--- tests/unit/_statistics/test_error_tracker.py
+++ tests/unit/_statistics/test_error_tracker.py
@@ -1,15 +1,15 @@
+import traceback
+
 from crawlee.statistics._error_tracker import ErrorTracker
 
 
 def test_error_tracker():
     error_tracker = ErrorTracker()
-    error_1 = ValueError("Some value error")
-    error_2 = ValueError("Some value error")
-    error_3 = ValueError("Another value error")
-
-    error_tracker.add(error_1)
-    error_tracker.add(error_2)
-    error_tracker.add(error_3)
+    for error in [ValueError("Some value error"), ValueError("Some value error"), ValueError("Another value error")]:
+        try:
+            raise error
+        except Exception:
+            error_tracker.add(error)
 
     assert error_tracker.total == 3
     assert error_tracker.unique_error_count == 1
