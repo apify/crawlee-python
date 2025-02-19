013166e34ad4b833883bf4246d683e32d32631cb
Max Bohomolov <34358312+Mantisus@users.noreply.github.com>
2025-02-19 13:26:17 +0200

    Update src/crawlee/browsers/_browser_pool.py
    Co-authored-by: Vlada Dusek <v.dusek96@gmail.com>

--- src/crawlee/browsers/_browser_pool.py
+++ src/crawlee/browsers/_browser_pool.py
@@ -114,8 +114,8 @@ class BrowserPool:
 
         Args:
             browser_type: The type of browser to launch ('chromium', 'firefox', or 'webkit').
-            user_data_dir: Path to a User Data Directory, which stores browser session data like cookies and local
-                storage.
+            user_data_dir: Path to a user data directory, which stores browser session data like cookies
+                and local storage.
             browser_launch_options: Keyword arguments to pass to the browser launch method. These options are provided
                 directly to Playwright's `browser_type.launch` method. For more details, refer to the Playwright
                 documentation: https://playwright.dev/python/docs/api/class-browsertype#browser-type-launch.
