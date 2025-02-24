dacc81062777b065afe538525a43142fbe675aee
Josef Prochazka <josef.prochazka@apify.com>
2025-02-24 13:09:48 +0100

    Add detailed message option.
    Do not port option for full trace - not used in JS version.

--- src/crawlee/statistics/_error_tracker.py
+++ src/crawlee/statistics/_error_tracker.py
@@ -5,63 +5,73 @@ from __future__ import annotations
 import traceback
 from collections import Counter, defaultdict
 from itertools import zip_longest
+from typing import Union
 
-ErrorMessageGroups = Counter[str|None]
-ErrorTypeGroups = dict[str|None, ErrorMessageGroups]
-ErrorStackTraceGroups = dict[str|None, ErrorTypeGroups]
+GroupName = Union[str, None]
+ErrorMessageGroups = Counter[GroupName]
+ErrorTypeGroups = dict[GroupName, ErrorMessageGroups]
+ErrorStackTraceGroups = dict[GroupName, ErrorTypeGroups]
 
 
 class ErrorTracker:
     """Track errors and aggregates their counts by similarity."""
 
-    def __init__(self,*,
-                 show_error_name: bool = True,
-                 show_stack_trace: bool = True,
-                 show_full_stack: bool = False, # TODO, what does it do in JS?
-                 show_error_message: bool = True,
-                 show_full_message: bool = False, # TODO, seems like only first line otherwise
-                 save_error_snapshots: bool = False, # TODO, what does it do in JS?
+    def __init__(
+        self,
+        *,
+        show_error_name: bool = True,
+        show_stack_trace: bool = True,
+        show_error_message: bool = True,
+        show_full_message: bool = False,
     ) -> None:
         self.show_error_name = show_error_name
         self.show_stack_trace = show_stack_trace
-        self.show_full_stack = show_full_stack
         self.show_error_message = show_error_message
+        if show_full_message and not show_error_message:
+            raise ValueError('`show_error_message` must be `True` if `show_full_message` is set to `True`')
         self.show_full_message = show_full_message
         self._errors: ErrorStackTraceGroups = defaultdict(lambda: defaultdict(Counter))
 
     def add(self, error: Exception) -> None:
         """Include an error in the statistics."""
-        # Check for similar
         error_group_name = error.__class__.__name__ if self.show_error_name else None
-        error_group_message = error.args[0] if self.show_error_message else None
-        if self.show_stack_trace:
-            error_traceback = traceback.extract_tb(error.__traceback__)
-            error_group_stack_trace = f'{error_traceback[0].filename}:{error_traceback[0].lineno}'
-            # More extraction if full stack...
-        else:
-            error_group_stack_trace = None
+        error_group_message = self._get_error_message(error)
+        error_group_stack_trace = self._get_traceback_text(error)
 
-        # TODO perform special message grouping
-        # First just exact match
-
-        # All groups, except the lowest level is matched.
+        # All groups, except the lowest level is matched exactly.
         specific_groups = self._errors[error_group_stack_trace][error_group_name]
 
+        # Lowest level is matched by similarity
         if error_group_message in specific_groups:
             # Exact match
             specific_groups.update([error_group_message])
         else:
-            for known_error_group_message in specific_groups:
-                if new_group_name:=self._create_generic_message(known_error_group_message, error_group_message):
+            for existing_error_group_message in specific_groups:
+                # Add to first group with similar text. Modify text with wildcard characters if necessary.
+                if new_error_group_message := self._create_generic_message(
+                    existing_error_group_message, error_group_message
+                ):
                     # Replace old name
-                    specific_groups[new_group_name]=specific_groups.pop(known_error_group_message)
+                    specific_groups[new_error_group_message] = specific_groups.pop(existing_error_group_message)
                     # Increment
-                    specific_groups.update([new_group_name])
+                    specific_groups.update([new_error_group_message])
                     break
             else:
                 # No similar message found. Create new group.
                 self._errors[error_group_stack_trace][error_group_name].update([error_group_message])
 
+    def _get_traceback_text(self, error: Exception) -> str | None:
+        if self.show_stack_trace:
+            error_traceback = traceback.extract_tb(error.__traceback__)
+            return f'{error_traceback[0].filename.split("/")[-1]}:{error_traceback[0].lineno}'
+        return None
+
+    def _get_error_message(self, error: Exception) -> str | None:
+        if self.show_error_message:
+            if self.show_full_message:
+                return str(error.args[0])
+            return str(error.args[0]).split('\n')[0]
+        return None
 
     @property
     def unique_error_count(self) -> int:
@@ -69,7 +79,7 @@ class ErrorTracker:
         unique_error_count = 0
         for stack_group in self._errors.values():
             for name_group in stack_group.values():
-                unique_error_count+=len(name_group)
+                unique_error_count += len(name_group)
         return unique_error_count
 
     @property
@@ -78,44 +88,49 @@ class ErrorTracker:
         error_count = 0
         for stack_group in self._errors.values():
             for name_group in stack_group.values():
-                error_count+=sum(name_group.values())
+                error_count += sum(name_group.values())
         return error_count
 
-    def get_most_popular_errors(self, n: int=3) -> str:
-        all_errors = Counter()
-        for stack_group_name, stack_group  in self._errors.items():
-            for name_group_name, name_group   in stack_group.items():
+    def get_most_common_errors(self, n: int = 3) -> list[tuple[str | None, int]]:
+        """Return n most common errors."""
+        all_errors: ErrorMessageGroups = Counter()
+        for stack_group_name, stack_group in self._errors.items():
+            for name_group_name, name_group in stack_group.items():
                 for message_group_name, count in name_group.items():
-                    all_errors[self._get_error_repr(
-                        stack_group_name, name_group_name, message_group_name)]=count
+                    all_errors[self._get_error_repr(stack_group_name, name_group_name, message_group_name)] = count
         return all_errors.most_common(n)
 
-
-
-    def _get_error_repr(self, stack: str, name: str, message: str) -> str:
+    def _get_error_repr(self, stack: str | None, name: str | None, message: str | None) -> str:
         """Get the most specific error representation."""
         stack_part = f'{stack}:' if stack else ''
         name_part = f'{name}:' if name else ''
         message_part = f'{message}' if message else ''
         return f'{stack_part}{name_part}{message_part}'
 
+    @staticmethod
+    def _create_generic_message(message_1: str | None, message_2: str | None) -> str | None:
+        """Create a generic error message from two messages, if they are similar enough.
 
-    def _create_generic_message(self, message_1: str, message_2: str) -> str:
+        Different parts of similar messages are replaced by `_`.
+        """
+        if message_1 is None or message_2 is None:
+            return None
+
         replacement_symbol = '_'
         replacement_count = 0
+
+        generic_message_parts = []
         message_1_parts = message_1.split(' ')
         message_2_parts = message_2.split(' ')
-        generic_message_parts = []
         parts_count = max(len(message_1_parts), len(message_2_parts))
 
         for message_1_part, message_2_part in zip_longest(message_1_parts, message_2_parts, fillvalue=''):
             if message_1_part != message_2_part:
                 generic_message_parts.append(replacement_symbol)
-                replacement_count+=1
-                if replacement_count >= parts_count/2:
+                replacement_count += 1
+                if replacement_count >= parts_count / 2:
                     # Messages are too different.
                     return ''
             else:
                 generic_message_parts.append(message_1_part)
         return ' '.join(generic_message_parts)
-
--- tests/unit/_statistics/test_error_tracker.py
+++ tests/unit/_statistics/test_error_tracker.py
@@ -5,16 +5,18 @@ import pytest
 from crawlee.statistics._error_tracker import ErrorTracker
 
 
-@pytest.mark.parametrize(('error_tracker', 'expected_unique_errors'), [
-    (ErrorTracker(), 4),
-    (ErrorTracker(show_stack_trace=False), 3),
-    (ErrorTracker(show_error_name=False), 3),
-    (ErrorTracker(show_error_message=False), 3),
-    (ErrorTracker(show_stack_trace=False, show_error_name=False), 2),
-    (ErrorTracker(show_stack_trace=False, show_error_message=False), 2),
-    (ErrorTracker(show_stack_trace=False, show_error_name=False, show_error_message=False), 1),
-
-])
+@pytest.mark.parametrize(
+    ('error_tracker', 'expected_unique_errors'),
+    [
+        (ErrorTracker(), 4),
+        (ErrorTracker(show_stack_trace=False), 3),
+        (ErrorTracker(show_error_name=False), 3),
+        (ErrorTracker(show_error_message=False), 3),
+        (ErrorTracker(show_stack_trace=False, show_error_name=False), 2),
+        (ErrorTracker(show_stack_trace=False, show_error_message=False), 2),
+        (ErrorTracker(show_stack_trace=False, show_error_name=False, show_error_message=False), 1),
+    ],
+)
 def test_error_tracker_counts(error_tracker: ErrorTracker, expected_unique_errors: int) -> None:
     """Use different settings of `error_tracker` and test unique errors count."""
 
@@ -22,7 +24,9 @@ def test_error_tracker_counts(error_tracker: ErrorTracker, expected_unique_error
         Exception('Some value error abc'),
         ValueError('Some value error abc'),  # Different type, different error
         ValueError('Some value error cde'),  # Same type and similar message to previous, considered the same.
-        ValueError('Another value error efg')  # Same type, but too different message to previous, considered different.
+        ValueError(
+            'Another value error efg'
+        ),  # Same type, but too different message to previous, considered different.
     ]:
         try:
             raise error  # Errors raised on same line
@@ -37,14 +41,19 @@ def test_error_tracker_counts(error_tracker: ErrorTracker, expected_unique_error
     assert error_tracker.total == 5
     assert error_tracker.unique_error_count == expected_unique_errors
 
-@pytest.mark.parametrize(('message_1', 'message_2', 'expected_generic_message'), [
-    ('Some error number 123', 'Some error number 456', 'Some error number _'),
-    ('Some error number 123 456', 'Some error number 123 456 789', 'Some error number 123 456 _'),
-    ('Some error number 0 0 0', 'Some error number 1 0 1', 'Some error number _ 0 _'),
-])
-def test_error_tracker_similar_messages(message_1: str, message_2: str, expected_generic_message: str) -> None:
-    """Test that similar messages collapse into same group with generic name that contains wildcards."""
-    line: int
+
+@pytest.mark.parametrize(
+    ('message_1', 'message_2', 'expected_generic_message'),
+    [
+        ('Some error number 123', 'Some error number 456', 'Some error number _'),
+        ('Some error number 123 456', 'Some error number 123 456 789', 'Some error number 123 456 _'),
+        ('Some error number 0 0 0', 'Some error number 1 0 1', 'Some error number _ 0 _'),
+    ],
+)
+def test_error_tracker_similar_messages_full_stack(
+    message_1: str, message_2: str, expected_generic_message: str
+) -> None:
+    """Test that similar messages collapse into same group with generic name that contains wildcard symbols."""
     error_tracker = ErrorTracker()
     for error in [
         KeyError(message_1),
@@ -60,10 +69,30 @@ def test_error_tracker_similar_messages(message_1: str, message_2: str, expected
             error_tracker.add(e)
             line = traceback.extract_tb(e.__traceback__)[0].lineno
 
-    errors = error_tracker.get_most_popular_errors()
-    assert errors[0][0] == f'{__file__}:{line}:KeyError:{message_1}'
+    file_name = __file__.split('/')[-1]
+    errors = error_tracker.get_most_common_errors()
+    assert errors[0][0] == f'{file_name}:{line}:KeyError:{message_1}'
     assert errors[0][1] == 3
-    assert errors[1][0] == f'{__file__}:{line}:ValueError:{expected_generic_message}'
+    assert errors[1][0] == f'{file_name}:{line}:ValueError:{expected_generic_message}'
     assert errors[1][1] == 2
-    assert errors[2][0] == f'{__file__}:{line}:RuntimeError:{message_2}'
+    assert errors[2][0] == f'{file_name}:{line}:RuntimeError:{message_2}'
     assert errors[2][1] == 1
+
+
+@pytest.mark.parametrize(
+    ('show_full_message', 'expected_message'),
+    [
+        (True, 'Error line 1\n Error line 2'),
+        (False, 'Error line 1'),
+    ],
+)
+def test_show_full_message(*, show_full_message: bool, expected_message: str) -> None:
+    """Test error message settings with both options of `show_full_message`."""
+    error_tracker = ErrorTracker(show_stack_trace=False, show_error_name=False, show_full_message=show_full_message)
+
+    try:
+        raise RuntimeError('Error line 1\n Error line 2')  # Errors raised on the same line
+    except Exception as e:
+        error_tracker.add(e)
+
+    assert error_tracker.get_most_common_errors()[0][0] == expected_message
