b003d21df6354f1a37551ec0cd9893a7d694b0c2
Max Bohomolov <moriturus7@gmail.com>
2025-03-05 00:45:22 +0000

    update template

--- src/crawlee/project_template/hooks/pre_gen_project.py
+++ src/crawlee/project_template/hooks/pre_gen_project.py
@@ -8,5 +8,5 @@ except OSError as exc:
     raise RuntimeError('You chose to use the Poetry package manager, but it does not seem to be installed') from exc
 
 if not re.match(r'Poetry \(version 1\..*\)', version):
-    raise RuntimeError(f'Poetry 1.x is required, but "{version}" is installed')
+    raise RuntimeError(f'Poetry below 3.x is required, but "{version}" is installed')
 # % endif
