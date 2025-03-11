31eedd78634599bf9e9fffab7019f397dd3f0cf7
Josef Prochazka <josef.prochazka@apify.com>
2025-03-11 15:35:27 +0100

    Temporary workaround for `browserforge` import time code execution

--- src/crawlee/fingerprint_suite/__init__.py
+++ src/crawlee/fingerprint_suite/__init__.py
@@ -1,3 +1,40 @@
+# ruff: noqa
+# Temporary fix until https://github.com/daijro/browserforge/pull/29 is merged
+from pathlib import Path
+from typing import Dict
+
+import apify_fingerprint_datapoints  # type:ignore[import-untyped]
+from browserforge import download
+from browserforge.download import DATA_FILES
+
+# Needed to be done before the import of code that does import time download
+download.DATA_DIRS: Dict[str, Path] = {  # type:ignore[misc]
+    'headers': apify_fingerprint_datapoints.get_header_network().parent,
+    'fingerprints': apify_fingerprint_datapoints.get_fingerprint_network().parent,
+}
+
+import browserforge.bayesian_network
+
+
+class BayesianNetwork(browserforge.bayesian_network.BayesianNetwork):
+    def __init__(self, path: Path) -> None:
+        if path.name in DATA_FILES['headers']:
+            path = download.DATA_DIRS['headers'] / path.name
+        else:
+            path = download.DATA_DIRS['fingerprints'] / path.name
+        super().__init__(path)
+
+
+browserforge.bayesian_network.BayesianNetwork = BayesianNetwork  # type:ignore[misc]
+
+import browserforge.headers.generator
+
+browserforge.headers.generator.DATA_DIR = download.DATA_DIRS['headers']
+import browserforge.fingerprints.generator
+
+browserforge.headers.generator.DATA_DIR = download.DATA_DIRS['fingerprints']
+# End of fix
+
 from ._browserforge_adapter import BrowserforgeFingerprintGenerator as DefaultFingerprintGenerator
 from ._fingerprint_generator import FingerprintGenerator
 from ._header_generator import HeaderGenerator
