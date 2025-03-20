832ef51dcdbf37cb856dd6f66a6049b97edd1457
Vlada Dusek <v.dusek96@gmail.com>
2025-03-20 14:21:14 +0100

    Update storage clients

--- src/crawlee/storage_clients/_base/_storage_client.py
+++ src/crawlee/storage_clients/_base/_storage_client.py
@@ -1,16 +1,48 @@
 from __future__ import annotations
 
-from dataclasses import dataclass
+from abc import ABC, abstractmethod
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
+    from pathlib import Path
+
     from ._dataset_client import DatasetClient
     from ._key_value_store_client import KeyValueStoreClient
     from ._request_queue_client import RequestQueueClient
 
 
-@dataclass
-class StorageClient:
-    dataset_client_class: type[DatasetClient]
-    key_value_store_client_class: type[KeyValueStoreClient]
-    request_queue_client_class: type[RequestQueueClient]
+class StorageClient(ABC):
+    """Base class for storage clients."""
+
+    @abstractmethod
+    async def open_dataset_client(
+        self,
+        *,
+        id: str | None,
+        name: str | None,
+        purge_on_start: bool,
+        storage_dir: Path,
+    ) -> DatasetClient:
+        """Open the dataset client."""
+
+    @abstractmethod
+    async def open_key_value_store_client(
+        self,
+        *,
+        id: str | None,
+        name: str | None,
+        purge_on_start: bool,
+        storage_dir: Path,
+    ) -> KeyValueStoreClient:
+        """Open the key-value store client."""
+
+    @abstractmethod
+    async def open_request_queue_client(
+        self,
+        *,
+        id: str | None,
+        name: str | None,
+        purge_on_start: bool,
+        storage_dir: Path,
+    ) -> RequestQueueClient:
+        """Open the request queue client."""
--- src/crawlee/storage_clients/_file_system/_storage_client.py
+++ src/crawlee/storage_clients/_file_system/_storage_client.py
@@ -1,13 +1,65 @@
 from __future__ import annotations
 
+from typing import TYPE_CHECKING
+
+from typing_extensions import override
+
 from crawlee.storage_clients._base import StorageClient
 
 from ._dataset_client import FileSystemDatasetClient
 from ._key_value_store import FileSystemKeyValueStoreClient
 from ._request_queue import FileSystemRequestQueueClient
 
-file_system_storage_client = StorageClient(
-    dataset_client_class=FileSystemDatasetClient,
-    key_value_store_client_class=FileSystemKeyValueStoreClient,
-    request_queue_client_class=FileSystemRequestQueueClient,
-)
+if TYPE_CHECKING:
+    from pathlib import Path
+
+
+class FileSystemStorageClient(StorageClient):
+    """File system storage client."""
+
+    @override
+    async def open_dataset_client(
+        self,
+        *,
+        id: str | None,
+        name: str | None,
+        purge_on_start: bool,
+        storage_dir: Path,
+    ) -> FileSystemDatasetClient:
+        dataset_client = await FileSystemDatasetClient.open(
+            id=id,
+            name=name,
+            storage_dir=storage_dir,
+        )
+
+        if purge_on_start:
+            await dataset_client.drop()
+            dataset_client = await FileSystemDatasetClient.open(
+                id=id,
+                name=name,
+                storage_dir=storage_dir,
+            )
+
+        return dataset_client
+
+    @override
+    async def open_key_value_store_client(
+        self,
+        *,
+        id: str | None,
+        name: str | None,
+        purge_on_start: bool,
+        storage_dir: Path,
+    ) -> FileSystemKeyValueStoreClient:
+        return FileSystemKeyValueStoreClient()
+
+    @override
+    async def open_request_queue_client(
+        self,
+        *,
+        id: str | None,
+        name: str | None,
+        purge_on_start: bool,
+        storage_dir: Path,
+    ) -> FileSystemRequestQueueClient:
+        return FileSystemRequestQueueClient()
