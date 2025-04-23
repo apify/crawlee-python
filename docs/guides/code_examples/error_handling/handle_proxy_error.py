f3e6616f0c1ad900e78f221ecaa5966c95665f8a
Max Bohomolov <moriturus7@gmail.com>
2025-04-23 15:45:30 +0000

    up example

--- docs/guides/code_examples/error_handling/handle_proxy_error.py
+++ docs/guides/code_examples/error_handling/handle_proxy_error.py
@@ -8,7 +8,7 @@ from crawlee.errors import ProxyError
 async def main() -> None:
     # Set how many session rotations will happen before calling the error handler
     # when ProxyError occurs
-    crawler = HttpCrawler(max_session_rotations=5)
+    crawler = HttpCrawler(max_session_rotations=5, max_request_retries=6)
 
     # For this example, we'll create a proxy error in our handler
     @crawler.router.default_handler
