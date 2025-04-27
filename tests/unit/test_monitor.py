4da960a2876176f4eee6869b00dc86c5e293aa2d
Joe_Ramaci <joeramaci@gmail.com>
2025-04-27 19:13:36 -0400

    changed gitignore

--- tests/unit/test_monitor.py
+++ tests/unit/test_monitor.py
@@ -5,13 +5,12 @@ from crawlee.monitor import pad_date
 
 
 class MonitorTests(unittest.TestCase):
-
     def test_format_date_time(self) -> None:
         date_object = datetime(2023, 12, 25, 14, 30, 5)
-        expected = "25/12/2023 14:30:5"
+        expected = '25/12/2023 14:30:5'
         self.assertEqual(monitor.format_date_time(date_object), expected)
         date_object2 = datetime(2025, 4, 10, 10, 10, 15)
-        expected = "10/4/2025 10:10:15"
+        expected = '10/4/2025 10:10:15'
         self.assertEqual(monitor.format_date_time(date_object2), expected)
 
     def test_bad_date_time(self) -> None:
@@ -23,23 +22,24 @@ class MonitorTests(unittest.TestCase):
             monitor.format_date_time(bad_date2)
 
     def test_pad_date(self) -> None:
-        self.assertEqual(pad_date(5, 2), "05")
-        self.assertEqual(pad_date(12345, 7), "0012345")
+        self.assertEqual(pad_date(5, 2), '05')
+        self.assertEqual(pad_date(12345, 7), '0012345')
 
-        self.assertEqual(pad_date("5", 4), "0005")
-        self.assertEqual(pad_date("Jean", 6), "00Jean")
+        self.assertEqual(pad_date('5', 4), '0005')
+        self.assertEqual(pad_date('Jean', 6), '00Jean')
 
-        self.assertEqual(pad_date(123, 2), "123")
-        self.assertEqual(pad_date("12345", 3), "12345")
+        self.assertEqual(pad_date(123, 2), '123')
+        self.assertEqual(pad_date('12345', 3), '12345')
 
         # edge cases
-        self.assertEqual(pad_date("", 5), "00000")
-        self.assertEqual(pad_date(0, 3), "000")
+        self.assertEqual(pad_date('', 5), '00000')
+        self.assertEqual(pad_date(0, 3), '000')
 
     # def test_log(self):
     #     test_line = "Test Hello :)"
     #     expected = "Test Hello :)"
     #     self.assertEqual(MonitorDisplay.log(test_line), expected)
+
 
 if __name__ == '__main__':
     unittest.main()
