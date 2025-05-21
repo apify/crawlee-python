34b08f72d9446297c5b98cbef8aaa031baf69eba
Josef Prochazka <josef.prochazka@apify.com>
2025-05-21 16:03:26 +0200

    Use USS instead of RSS to estimate children process memory usage Add test

--- src/crawlee/_utils/system.py
+++ src/crawlee/_utils/system.py
@@ -4,7 +4,7 @@ import os
 from contextlib import suppress
 from datetime import datetime, timezone
 from logging import getLogger
-from typing import Annotated
+from typing import Annotated, Any
 
 import psutil
 from pydantic import BaseModel, ConfigDict, Field, PlainSerializer, PlainValidator
@@ -81,13 +81,16 @@ def get_memory_info() -> MemoryInfo:
 
     # Retrieve the Resident Set Size (RSS) of the current process. RSS is the portion of memory
     # occupied by a process that is held in RAM.
-    current_size_bytes = int(current_process.memory_info().rss)
+    # Use RSS as a conservative estimate that can be overestimating due to including shared memory as opposed to USS.
+    current_size_bytes = int(current_process.memory_full_info().rss)
 
+    # Sum memory usage by all children processes, try to exclude shared memory from the sum if allowed by OS.
     for child in current_process.children(recursive=True):
         # Ignore any NoSuchProcess exception that might occur if a child process ends before we retrieve
         # its memory usage.
         with suppress(psutil.NoSuchProcess):
-            current_size_bytes += int(child.memory_info().rss)
+            # In the case of children try to estimate memory usage from USS to avoid overestimation due to shared memory
+            current_size_bytes += _get_used_memory(child.memory_full_info())
 
     total_size_bytes = psutil.virtual_memory().total
 
@@ -95,3 +98,20 @@ def get_memory_info() -> MemoryInfo:
         total_size=ByteSize(total_size_bytes),
         current_size=ByteSize(current_size_bytes),
     )
+
+
+def _get_used_memory(memory_full_info: Any) -> int:
+    """Get the most suitable available used memory metric.
+
+    `Unique Set Size (USS)` is the memory which is unique to a process and which would be freed if the process was
+        terminated right now. It should be available on Linux, macOS, Windows.
+    `Resident Set Size (RSS)` is the non-swapped physical memory a process has used; it includes shared memory. It
+        should be available everywhere.
+    """
+    try:
+        # Overwhelming majority use-case (Linux, macOS, Windows)
+        return int(memory_full_info.uss)
+    except AttributeError:
+        # Very rare use-case
+        # Memory usage estimation can overestimate memory usage due to shared memory inclusion.
+        return int(memory_full_info.rss)
