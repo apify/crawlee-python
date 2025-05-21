e9f5f25908ec440cf495f04bcc314446d9d7fbfa
Max Bohomolov <moriturus7@gmail.com>
2025-05-21 11:37:55 +0000

    set `WindowsSelectorEventLoopPolicy` only for curl-impersonate without `playwright`

--- src/crawlee/project_template/{{cookiecutter.project_name}}/{{cookiecutter.__package_name}}/__main__.py
+++ src/crawlee/project_template/{{cookiecutter.project_name}}/{{cookiecutter.__package_name}}/__main__.py
@@ -1,12 +1,15 @@
 import asyncio
+# % if cookiecutter.http_client == 'curl-impersonate' and 'playwright' not in cookiecutter.crawler_type
 import platform
-
+# % endif
+{{ '' }}
 from .main import main
 
-
 if __name__ == '__main__':
+    # % if cookiecutter.http_client == 'curl-impersonate' and 'playwright' not in cookiecutter.crawler_type
     if platform.system() == 'Windows':
-        # This mitigates a warning raised by curl-cffi. If you do not need to use curl-impersonate, you may remove this.
+        # This mitigates a warning raised by curl-cffi.
         asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
-
+    # % endif
+{{ '' }}
     asyncio.run(main())
