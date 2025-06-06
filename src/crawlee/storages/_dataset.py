9f10b955c6d8c0d82940ad1c6bec0be6d5274565
Vlada Dusek <v.dusek96@gmail.com>
2025-06-06 15:37:46 +0200

    rm code duplication for open methods

--- src/crawlee/storages/_dataset.py
+++ src/crawlee/storages/_dataset.py
@@ -12,6 +12,7 @@ from crawlee._utils.file import export_csv_to_stream, export_json_to_stream
 
 from ._base import Storage
 from ._key_value_store import KeyValueStore
+from ._utils import open_storage_instance
 
 if TYPE_CHECKING:
     from collections.abc import AsyncIterator
@@ -109,39 +110,18 @@ class Dataset(Storage):
         configuration: Configuration | None = None,
         storage_client: StorageClient | None = None,
     ) -> Dataset:
-        if id and name:
-            raise ValueError('Only one of "id" or "name" can be specified, not both.')
-
-        # Check for default instance if no id or name provided
-        if id is None and name is None and cls._default_instance is not None:
-            return cls._default_instance
-
-        # Check if the dataset is already cached
-        if id is not None and id in cls._cache_by_id:
-            return cls._cache_by_id[id]
-        if name is not None and name in cls._cache_by_name:
-            return cls._cache_by_name[name]
-
         configuration = service_locator.get_configuration() if configuration is None else configuration
         storage_client = service_locator.get_storage_client() if storage_client is None else storage_client
-
-        client = await storage_client.open_dataset_client(
+        return await open_storage_instance(
+            cls,
             id=id,
             name=name,
             configuration=configuration,
+            cache_by_id=cls._cache_by_id,
+            cache_by_name=cls._cache_by_name,
+            default_instance_attr='_default_instance',
+            client_opener=storage_client.open_dataset_client,
         )
-        dataset = cls(client)
-
-        # Cache the dataset instance by ID and name
-        cls._cache_by_id[dataset.id] = dataset
-        if dataset.name is not None:
-            cls._cache_by_name[dataset.name] = dataset
-
-        # Store as default instance if neither id nor name was provided
-        if id is None and name is None:
-            cls._default_instance = dataset
-
-        return dataset
 
     @override
     async def drop(self) -> None:
