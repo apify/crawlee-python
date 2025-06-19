c14cf376079cc201bbcee6b99523bf0f5f787156
Josef Procházka <josef.prochazka@apify.com>
2025-06-19 11:54:54 +0200

    Update tests/unit/_statistics/test_request_processing_record.py
    Co-authored-by: Vlada Dusek <v.dusek96@gmail.com>

--- tests/unit/_statistics/test_request_processing_record.py
+++ tests/unit/_statistics/test_request_processing_record.py
@@ -4,7 +4,7 @@ from crawlee.statistics._statistics import RequestProcessingRecord
 
 
 def test_tracking_time_resolution() -> None:
-    """Test that `RequestProcessingRecord` tracks time with sufficient resolution
+    """Test that `RequestProcessingRecord` tracks time with sufficient resolution.
 
     This is generally not an issue on Linux, but on Windows some packages in older Python versions might be using system
     timers with not so granular resolution - some sources estimate 15ms. This test will start failing on Windows
