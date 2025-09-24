70477c95e2765d5ff706c5f6b61781c71fa8294b
Josef Prochazka <josef.prochazka@apify.com>
2025-09-24 10:19:45 +0200

    Review comments

--- src/crawlee/_utils/raise_if_too_many_kwargs.py
+++ src/crawlee/_utils/raise_if_too_many_kwargs.py
@@ -3,7 +3,11 @@ from typing import Any
 
 
 def raise_if_too_many_kwargs(max_kwargs: int = 1, **kwargs: Any) -> None:
-    """Raise ValueError if there are more kwargs then max_kwargs."""
+    """Raise ValueError if there are more non-None kwargs then max_kwargs."""
     if len(kwargs) - Counter(kwargs.values())[None] > max_kwargs:
         kwargs_names = [f'"{kwarg_name}"' for kwarg_name in kwargs]
-        raise ValueError(f'Only one of {", ".join(kwargs_names)} can be specified, not multiple.')
+        used_kwargs_names = [f'"{kwarg_name}"' for kwarg_name, value in kwargs.items() if value is not None]
+        raise ValueError(
+            f'Only one of {", ".join(kwargs_names)} can be specified, but following arguments were '
+            f'specified: {", ".join(used_kwargs_names)}.'
+        )
--- tests/unit/storages/test_request_queue.py
+++ tests/unit/storages/test_request_queue.py
@@ -76,7 +76,11 @@ async def test_open_with_id_and_name(
     storage_client: StorageClient,
 ) -> None:
     """Test that open() raises an error when both id and name are provided."""
-    with pytest.raises(ValueError, match=r'Only one of "id", "name", "alias" can be specified, not multiple.'):
+    with pytest.raises(
+        ValueError,
+        match=r'Only one of "id", "name", "alias" can be specified, but following arguments '
+        r'were specified: "id", "name".',
+    ):
         await RequestQueue.open(
             id='some-id',
             name='some-name',
@@ -674,7 +678,11 @@ async def test_alias_with_id_error(
     storage_client: StorageClient,
 ) -> None:
     """Test that providing both alias and id raises error."""
-    with pytest.raises(ValueError, match=r'Only one of "id", "name", "alias" can be specified, not multiple.'):
+    with pytest.raises(
+        ValueError,
+        match=r'Only one of "id", "name", "alias" can be specified, but following arguments '
+        r'were specified: "id", "alias".',
+    ):
         await RequestQueue.open(
             id='some-id',
             alias='some-alias',
@@ -686,7 +694,11 @@ async def test_alias_with_name_error(
     storage_client: StorageClient,
 ) -> None:
     """Test that providing both alias and name raises error."""
-    with pytest.raises(ValueError, match=r'Only one of "id", "name", "alias" can be specified, not multiple.'):
+    with pytest.raises(
+        ValueError,
+        match=r'Only one of "id", "name", "alias" can be specified, but following arguments '
+        r'were specified: "name", "alias".',
+    ):
         await RequestQueue.open(
             name='some-name',
             alias='some-alias',
