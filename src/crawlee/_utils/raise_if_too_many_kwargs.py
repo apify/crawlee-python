1944d402e05a03e7877436a9bbf6018471369677
Josef Prochazka <josef.prochazka@apify.com>
2025-09-26 10:10:54 +0200

    Review comment

--- src/crawlee/_utils/raise_if_too_many_kwargs.py
+++ src/crawlee/_utils/raise_if_too_many_kwargs.py
@@ -1,13 +1,12 @@
-from collections import Counter
 from typing import Any
 
 
 def raise_if_too_many_kwargs(max_kwargs: int = 1, **kwargs: Any) -> None:
     """Raise ValueError if there are more non-None kwargs then max_kwargs."""
-    if len(kwargs) - Counter(kwargs.values())[None] > max_kwargs:
-        kwargs_names = [f'"{kwarg_name}"' for kwarg_name in kwargs]
-        used_kwargs_names = [f'"{kwarg_name}"' for kwarg_name, value in kwargs.items() if value is not None]
+    none_kwargs_names = [f'"{kwarg_name}"' for kwarg_name, value in kwargs.items() if value is not None]
+    if len(none_kwargs_names) > max_kwargs:
+        all_kwargs_names = [f'"{kwarg_name}"' for kwarg_name in kwargs]
         raise ValueError(
-            f'Only one of {", ".join(kwargs_names)} can be specified, but following arguments were '
-            f'specified: {", ".join(used_kwargs_names)}.'
+            f'Only one of {", ".join(all_kwargs_names)} can be specified, but following arguments were '
+            f'specified: {", ".join(none_kwargs_names)}.'
         )
