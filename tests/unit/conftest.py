25fc88234844eef38de7946f3fdc18c59ddec2fd
Max Bohomolov <moriturus7@gmail.com>
2025-10-23 12:21:57 +0000

    test 7

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -207,7 +207,7 @@ def redirect_server_url(redirect_http_server: TestServer) -> URL:
 )
 async def http_client(request: pytest.FixtureRequest) -> AsyncGenerator[HttpClient, None]:
     class_client: type[HttpClient]
-    # test 6
+    # test 7
     if request.param == 'curl':
         class_client = CurlImpersonateHttpClient
         kwargs: dict[str, Any] = {'http_version': CurlHttpVersion.V1_1}
