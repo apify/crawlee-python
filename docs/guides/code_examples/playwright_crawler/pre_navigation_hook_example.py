fe225422cc7c0026a95373f592135b3e9ef5d834
Max Bohomolov <moriturus7@gmail.com>
2026-03-18 01:21:16 +0000

    update doc

--- docs/guides/code_examples/playwright_crawler/pre_navigation_hook_example.py
+++ docs/guides/code_examples/playwright_crawler/pre_navigation_hook_example.py
@@ -17,14 +17,12 @@ async def main() -> None:
         await context.enqueue_links()
 
     @crawler.pre_navigation_hook
-    async def log_navigation_url(context: PlaywrightPreNavCrawlingContext) -> None:
+    async def configure_page(context: PlaywrightPreNavCrawlingContext) -> None:
         context.log.info(f'Navigating to {context.request.url} ...')
 
-        # will set a timeout for all navigation methods
-        context.page.set_default_navigation_timeout(600_000)
-
-        # will set the page size before you go to the target URL
-        await context.page.set_viewport_size({'width': 1280, 'height': 1024})
+        # block stylesheets, images, fonts and other static assets
+        # to speed up page loading
+        await context.block_requests()
 
     # Run the crawler with the initial list of URLs.
     await crawler.run(['https://crawlee.dev'])
