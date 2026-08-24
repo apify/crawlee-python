d4e148490160371c2d4ec2ae768adc6456969c0b
Vlada Dusek <v.dusek96@gmail.com>
2026-08-24 15:31:44 +0200

    test(httpx): Assert the `python-httpx2` user agent

--- tests/unit/http_clients/test_httpx.py
+++ tests/unit/http_clients/test_httpx.py
@@ -162,6 +162,6 @@ async def test_no_headers_without_generator(server_url: URL) -> None:
         response = await client.send_request(str(server_url / 'headers'))
         headers = await read_json(response)
 
-    assert 'python-httpx' in headers['user-agent']
+    assert 'python-httpx2' in headers['user-agent']
     assert headers['accept'] == '*/*'
     assert 'accept-language' not in headers
